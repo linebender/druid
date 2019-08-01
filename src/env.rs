f4ffb65349aefba03c2a32eb6f3f0af6d36bba0c
Raph Levien <raph.levien@gmail.com>
2019-08-01 16:36:55 -0700

    More low level plumbing on env

--- src/env.rs
+++ src/env.rs
@@ -28,25 +28,111 @@ use crate::Data;
 // maybe we should use Rc instead of Arc.
 use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct Env(Arc<EnvImpl>);
 
 pub struct EnvImpl {
     theme: EnvValue,
+    // This is always a map, maybe should be a map here.
+    values: EnvValue,
 }
 
 #[derive(Clone)]
 pub enum EnvValue {
     Float(f64),
+    Bool(bool),
+    Color(Color),
     String(String),
     List(Arc<Vec<EnvValue>>),
     Map(Arc<HashMap<String, EnvValue>>),
     Null,
 }
 
+impl Env {
+    pub fn from_theme(theme: EnvValue) -> Env {
+        Env(Arc::new(EnvImpl {
+            theme,
+            values: EnvValue::empty_map(),
+        }))
+    }
+
+    /// Get a theme value.
+    ///
+    /// The value will be EnvValue::Null if the key is not present.
+    // TODO: make key more flexible
+    pub fn theme_value(&self, key: &str) -> &EnvValue {
+        &self.0.theme[key]
+    }
+
+    /// Get a theme color.
+    ///
+    /// Prints a debug warning and returns a garish color if the key
+    /// isn't present or is of the wrong type.
+    pub fn theme_color(&self, key: &str) -> Color {
+        if let Some(c) = self.theme_value(key).as_color() {
+            c
+        } else {
+            println!("missing theme color for {}", key);
+            Color::rgb24(0xff_00_ff)
+        }
+    }
+
+    /// Get a theme float value.
+    ///
+    /// Prints a debug warning and returns 0.0 if the key isn't present
+    /// or is of the wrong type.
+    ///
+    /// Discussion question: would it be better to panic? Should we have
+    /// a more systematic error logging infrastructure? This seems good
+    /// enough for now.
+    pub fn theme_float(&self, key: &str) -> f64 {
+        if let Some(f) = self.theme_value(key).as_float() {
+            f
+        } else {
+            println!("missing theme float for {}", key);
+            0.0
+        }
+    }
+
+    pub fn update(&self, key: &str, value: impl Into<EnvValue>) -> Env {
+        // Note: this always does the clone, we might want a variant that
+        // takes `self` so doesn't.
+        let values = self.0.values.to_owned().update(key, value.into());
+        Env(Arc::new(EnvImpl {
+            theme: self.0.theme.clone(),
+            values,
+        }))
+    }
+}
+
+impl Data for Env {
+    fn same(&self, other: &Env) -> bool {
+        self.0.theme.same(&other.0.theme) && self.0.values.same(&other.0.values)
+    }
+}
+
 impl EnvValue {
+    pub fn empty_map() -> EnvValue {
+        EnvValue::Map(Arc::new(HashMap::new()))
+    }
+
     pub fn as_float(&self) -> Option<f64> {
         match self {
             EnvValue::Float(f) => Some(*f),
+            _ => None,
+        }
+    }
+
+    pub fn as_bool(&self) -> Option<bool> {
+        match self {
+            EnvValue::Bool(b) => Some(*b),
+            _ => None,
+        }
+    }
+
+    pub fn as_color(&self) -> Option<Color> {
+        match self {
+            EnvValue::Color(c) => Some(c.to_owned()),
             _ => None,
         }
     }
@@ -58,15 +144,47 @@ impl EnvValue {
         }
     }
 
-    pub fn update(self, key: impl Into<String>, value: EnvValue) -> EnvValue {
+    // Note: for the key here, we probably want to do something like the KeyPath
+    // that's in value.rs.
+    pub fn update(self, key: impl Into<String>, value: impl Into<EnvValue>) -> EnvValue {
         match self {
             EnvValue::Map(mut m) => {
                 let mut m_ref = Arc::make_mut(&mut m);
-                m_ref.insert(key.into(), value);
+                m_ref.insert(key.into(), value.into());
                 EnvValue::Map(m)
             }
             _ => EnvValue::Null,
         }
+    }
+}
+
+impl From<f64> for EnvValue {
+    fn from(x: f64) -> EnvValue {
+        EnvValue::Float(x)
+    }
+}
+
+impl From<bool> for EnvValue {
+    fn from(x: bool) -> EnvValue {
+        EnvValue::Bool(x)
+    }
+}
+
+impl From<Color> for EnvValue {
+    fn from(x: Color) -> EnvValue {
+        EnvValue::Color(x)
+    }
+}
+
+impl<'a> From<&'a str> for EnvValue {
+    fn from(x: &str) -> EnvValue {
+        EnvValue::String(x.to_owned())
+    }
+}
+
+impl<'a> From<String> for EnvValue {
+    fn from(x: String) -> EnvValue {
+        EnvValue::String(x)
     }
 }
 
@@ -92,6 +210,12 @@ impl Data for EnvValue {
     fn same(&self, other: &EnvValue) -> bool {
         match (self, other) {
             (EnvValue::Float(f1), EnvValue::Float(f2)) => f1.same(f2),
+            (EnvValue::Color(c1), EnvValue::Color(c2)) => {
+                // Note: when `Color` gets richer, this also needs to change.
+                // Consider implementing equality (or sameness, considering the
+                // possibility of float NaN values) on `piet::Color`.
+                c1.as_rgba32() == c2.as_rgba32()
+            }
             (EnvValue::String(s1), EnvValue::String(s2)) => s1 == s2,
             (EnvValue::List(l1), EnvValue::List(l2)) => l1.same(l2),
             (EnvValue::Map(m1), EnvValue::Map(m2)) => {
