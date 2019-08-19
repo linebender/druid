6e0e5e99e5e4f6b70da6a8aefee069e78c860770
Colin Rofls <colin@cmyr.net>
2019-08-19 16:18:59 -0400

    Update druid-shell menus API

--- druid-shell/src/mac/menu.rs
+++ druid-shell/src/mac/menu.rs
@@ -13,11 +13,14 @@
 // limitations under the License.
 
 //! macOS implementation of menus.
-use crate::keycodes::{KeySpec, MenuKey};
+
 use crate::util::make_nsstring;
-use cocoa::appkit::{NSMenu, NSMenuItem};
+use cocoa::appkit::{NSEventModifierFlags, NSMenu, NSMenuItem};
 use cocoa::base::{id, nil, NO};
 use cocoa::foundation::NSAutoreleasePool;
+
+use crate::hotkey::{HotKey, KeyCompare};
+use crate::keyboard::{KeyCode, KeyModifiers};
 
 pub struct Menu {
     pub menu: id,
@@ -25,14 +28,13 @@ pub struct Menu {
 
 /// Make a string in the syntax expected as the keyEquivalent argument to
 /// NSMenuItem initWithTitle:action:keyEquivalent:
-fn make_key_equivalent(key: impl Into<MenuKey>) -> String {
-    // TODO: handle modifiers and other fun things
-    let key = key.into();
-    match key.key {
-        KeySpec::Char(c) => c.to_string(),
-        KeySpec::None => "".to_string(),
-    }
-}
+//fn make_key_equivalent(key: &HotKey) -> String {
+//// TODO: handle modifiers and other fun things
+//match key.key {
+//KeySpec::Char(c) => c.to_string(),
+//KeySpec::None => "".to_string(),
+//}
+//}
 
 /// Strip the access keys from the menu strong.
 ///
@@ -54,8 +56,8 @@ fn strip_access_key(raw_menu_text: &str) -> String {
     result
 }
 
-fn make_menu_item(id: u32, text: &str, key: impl Into<MenuKey>) -> id {
-    let key_equivalent = make_key_equivalent(key);
+fn make_menu_item(_id: u32, text: &str, key: Option<&HotKey>) -> id {
+    let key_equivalent = key.map(HotKey::key_equivalent).unwrap_or("");
     let stripped_text = strip_access_key(text);
     unsafe {
         let item = NSMenuItem::alloc(nil)
@@ -65,7 +67,9 @@ fn make_menu_item(id: u32, text: &str, key: impl Into<MenuKey>) -> id {
                 make_nsstring(&key_equivalent),
             )
             .autorelease();
-        msg_send![item, setTag: id as isize];
+        if let Some(mask) = key.map(HotKey::key_modifier_mask) {
+            msg_send![item, setKeyEquivalentModifierMask: mask];
+        }
         item
     }
 }
@@ -80,15 +84,19 @@ impl Menu {
     }
 
     // TODO: how do we use the text here?
-    pub fn add_dropdown(&mut self, menu: Menu, _text: &str) {
+    pub fn add_dropdown(&mut self, menu: Menu, text: &str) {
         unsafe {
-            let menu_item = NSMenuItem::new(nil).autorelease();
+            let menu_item = NSMenuItem::alloc(nil);
+            let title = make_nsstring(text);
+            msg_send![menu_item, setTitle: title];
+            msg_send![menu.menu, setTitle: title];
             menu_item.setSubmenu_(menu.menu);
             self.menu.addItem_(menu_item);
         }
     }
 
-    pub fn add_item(&mut self, id: u32, text: &str, key: impl Into<MenuKey>) {
+    pub fn add_item(&mut self, id: u32, text: &str, key: Option<&HotKey>) {
+        eprintln!("{}: {}", id, text);
         let menu_item = make_menu_item(id, text, key);
         unsafe {
             self.menu.addItem_(menu_item);
@@ -109,8 +117,78 @@ impl Default for Menu {
         let mut menu = Menu::new();
         // this one is our actual menu
         let mut submenu = Menu::new();
-        submenu.add_item(1, "Quit", 'q');
+        //submenu.add_item(1, "Quit", 'q');
         menu.add_dropdown(submenu, "Application");
         menu
+    }
+}
+impl HotKey {
+    /// Return the string value of this hotkey, for use with Cocoa `NSResponder`
+    /// objects.
+    ///
+    /// Returns the empty string if no key equivalent is known.
+    fn key_equivalent(&self) -> &str {
+        match self.key {
+            KeyCompare::Text(t) => t,
+
+            // from NSText.h
+            KeyCompare::Code(KeyCode::Return) => "\u{0003}",
+            KeyCompare::Code(KeyCode::Backspace) => "\u{0008}",
+            KeyCompare::Code(KeyCode::Delete) => "\u{007f}",
+            // from NSEvent.h
+            KeyCompare::Code(KeyCode::Insert) => "\u{F727}",
+            KeyCompare::Code(KeyCode::Home) => "\u{F729}",
+            KeyCompare::Code(KeyCode::End) => "\u{F72B}",
+            KeyCompare::Code(KeyCode::PageUp) => "\u{F72C}",
+            KeyCompare::Code(KeyCode::PageDown) => "\u{F72D}",
+            KeyCompare::Code(KeyCode::PrintScreen) => "\u{F72E}",
+            KeyCompare::Code(KeyCode::ScrollLock) => "\u{F72F}",
+            KeyCompare::Code(KeyCode::ArrowUp) => "\u{F700}",
+            KeyCompare::Code(KeyCode::ArrowDown) => "\u{F701}",
+            KeyCompare::Code(KeyCode::ArrowLeft) => "\u{F702}",
+            KeyCompare::Code(KeyCode::ArrowRight) => "\u{F703}",
+            KeyCompare::Code(KeyCode::F1) => "\u{F704}",
+            KeyCompare::Code(KeyCode::F2) => "\u{F705}",
+            KeyCompare::Code(KeyCode::F3) => "\u{F706}",
+            KeyCompare::Code(KeyCode::F4) => "\u{F707}",
+            KeyCompare::Code(KeyCode::F5) => "\u{F708}",
+            KeyCompare::Code(KeyCode::F6) => "\u{F709}",
+            KeyCompare::Code(KeyCode::F7) => "\u{F70A}",
+            KeyCompare::Code(KeyCode::F8) => "\u{F70B}",
+            KeyCompare::Code(KeyCode::F9) => "\u{F70C}",
+            KeyCompare::Code(KeyCode::F10) => "\u{F70D}",
+            //KeyCompare::Code(KeyCode::F11)            => "\u{F70E}",
+            //KeyCompare::Code(KeyCode::F12)            => "\u{F70F}",
+            //KeyCompare::Code(KeyCode::F13)            => "\u{F710}",
+            //KeyCompare::Code(KeyCode::F14)            => "\u{F711}",
+            //KeyCompare::Code(KeyCode::F15)            => "\u{F712}",
+            //KeyCompare::Code(KeyCode::F16)            => "\u{F713}",
+            //KeyCompare::Code(KeyCode::F17)            => "\u{F714}",
+            //KeyCompare::Code(KeyCode::F18)            => "\u{F715}",
+            //KeyCompare::Code(KeyCode::F19)            => "\u{F716}",
+            //KeyCompare::Code(KeyCode::F20)            => "\u{F717}",
+            KeyCompare::Code(other) => {
+                eprintln!("no key equivalent for {:?}", other);
+                ""
+            }
+        }
+    }
+
+    fn key_modifier_mask(&self) -> NSEventModifierFlags {
+        let mods: KeyModifiers = self.mods.into();
+        let mut flags = NSEventModifierFlags::empty();
+        if mods.shift {
+            flags.insert(NSEventModifierFlags::NSShiftKeyMask);
+        }
+        if mods.meta {
+            flags.insert(NSEventModifierFlags::NSCommandKeyMask);
+        }
+        if mods.alt {
+            flags.insert(NSEventModifierFlags::NSAlternateKeyMask);
+        }
+        if mods.ctrl {
+            flags.insert(NSEventModifierFlags::NSControlKeyMask);
+        }
+        flags
     }
 }
--- druid-shell/src/windows/menu.rs
+++ druid-shell/src/windows/menu.rs
@@ -21,7 +21,7 @@ use winapi::shared::basetsd::*;
 use winapi::shared::windef::*;
 use winapi::um::winuser::*;
 
-use crate::keycodes::MenuKey;
+use crate::hotkey::{HotKey, KeyCompare};
 use crate::util::ToWide;
 
 /// A menu object, which can be either a top-level menubar or a
@@ -56,11 +56,15 @@ impl Menu {
     /// probably want to change that so we can manipulate it later.
     ///
     /// The `text` field has all the fun behavior of winapi CreateMenu.
-    pub fn add_dropdown(&mut self, menu: Menu, text: &str) {
+    pub fn add_dropdown(&mut self, menu: Menu, text: &str, enabled: bool) {
         unsafe {
+            let mut flags = MF_POPUP;
+            if !enabled {
+                flags &= MF_GRAYED;
+            }
             AppendMenuW(
                 self.hmenu,
-                MF_POPUP,
+                flags,
                 menu.into_hmenu() as UINT_PTR,
                 text.to_wide().as_ptr(),
             );
@@ -68,15 +72,24 @@ impl Menu {
     }
 
     /// Add an item to the menu.
-    pub fn add_item(&mut self, id: u32, text: &str, _key: impl Into<MenuKey>) {
+    pub fn add_item(
+        &mut self,
+        id: u32,
+        text: &str,
+        _key: Option<&HotKey>,
+        enabled: bool,
+        selected: bool,
+    ) {
         // TODO: actually wire up accelerators for key.
         unsafe {
-            AppendMenuW(
-                self.hmenu,
-                MF_STRING,
-                id as UINT_PTR,
-                text.to_wide().as_ptr(),
-            );
+            let mut flags = MF_STRING;
+            if !enabled {
+                flags &= MF_GRAYED;
+            }
+            if !selected {
+                flags &= MF_CHECKED;
+            }
+            AppendMenuW(self.hmenu, flags, id as UINT_PTR, text.to_wide().as_ptr());
         }
     }
 
