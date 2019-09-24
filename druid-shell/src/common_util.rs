8387aa2012bc21ddc8d6682b47c8793871e3b828
Andrea Cognolato <andrecogno@hotmail.it>
2019-09-24 16:06:18 +0200

    Add copyright and top level docs

--- druid-shell/src/common_util.rs
+++ druid-shell/src/common_util.rs
@@ -1,3 +1,19 @@
+// Copyright 2019 The xi-editor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Common functions used by the backends
+
 /// Strip the access keys from the menu string.
 ///
 /// Changes "E&xit" to "Exit". Actual ampersands are escaped as "&&".
