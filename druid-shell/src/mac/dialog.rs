3cb5700c800c914ede183e16f1ae48d0b0d18993
Colin Rofls <colin@cmyr.net>
2019-09-25 11:41:51 -0400

    [mac] Implement open panel options

--- druid-shell/src/mac/dialog.rs
+++ druid-shell/src/mac/dialog.rs
@@ -18,18 +18,40 @@
 
 use std::ffi::OsString;
 
-use cocoa::base::id;
-use cocoa::foundation::NSInteger;
+use cocoa::base::{id, nil, YES};
+use cocoa::foundation::{NSArray, NSInteger};
 
 use crate::dialog::FileDialogOptions;
-use crate::util::from_nsstring;
+use crate::util::{from_nsstring, make_nsstring};
 
 const NSModalResponseOK: NSInteger = 1;
 const NSModalResponseCancel: NSInteger = 0;
 
-pub(crate) unsafe fn show_open_file_dialog_sync(_options: FileDialogOptions) -> Option<OsString> {
+pub(crate) unsafe fn show_open_file_dialog_sync(options: FileDialogOptions) -> Option<OsString> {
     let nsopenpanel = class!(NSOpenPanel);
     let panel: id = msg_send![nsopenpanel, openPanel];
+
+    // set options
+
+    if options.show_hidden {
+        msg_send![panel, setShowsHiddenFiles: YES];
+    }
+
+    // A vector of NSStrings. this must outlive `nsarray_allowed_types`.
+    let allowed_types = options.allowed_types.as_ref().map(|specs| {
+        specs
+            .iter()
+            .flat_map(|spec| spec.extensions.iter().map(|s| make_nsstring(s)))
+            .collect::<Vec<_>>()
+    });
+
+    let nsarray_allowed_types = allowed_types
+        .as_ref()
+        .map(|types| NSArray::arrayWithObjects(nil, types.as_slice()));
+    if let Some(nsarray) = nsarray_allowed_types {
+        msg_send![panel, setAllowedFileTypes: nsarray];
+    }
+
     let result: NSInteger = msg_send![panel, runModal];
     match result {
         NSModalResponseOK => {
