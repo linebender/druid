c93e723f6cbd34985ad8e76a0ee2813468b8a825
ForLoveOfCats <ForLoveOfCats@vivaldi.net>
2019-11-09 15:24:02 -0500

    Add scrollbar width and padding to theme

--- druid/src/theme.rs
+++ druid/src/theme.rs
@@ -42,12 +42,16 @@ pub const BORDERED_WIDGET_HEIGHT: Key<f64> = Key::new("bordered_widget_height");
 
 pub const SCROLL_BAR_COLOR: Key<Color> = Key::new("scroll_bar_color");
 pub const SCROLL_BAR_BORDER_COLOR: Key<Color> = Key::new("scroll_bar_border_color");
+pub const SCROLL_BAR_WIDTH: Key<f64> = Key::new("scroll_bar_width");
+pub const SCROLL_BAR_PAD: Key<f64> = Key::new("scroll_bar_pad");
 
 /// An initial theme.
 pub fn init() -> Env {
     let mut env = Env::default()
         .adding(WINDOW_BACKGROUND_COLOR, Color::rgb8(0x29, 0x29, 0x29))
+
         .adding(LABEL_COLOR, Color::rgb8(0xf0, 0xf0, 0xea))
+
         .adding(PRIMARY_LIGHT, Color::rgb8(0x5c, 0xc4, 0xff))
         .adding(PRIMARY_DARK, Color::rgb8(0x00, 0x8d, 0xdd))
         .adding(BACKGROUND_LIGHT, Color::rgb8(0x3a, 0x3a, 0x3a))
@@ -60,11 +64,15 @@ pub fn init() -> Env {
         .adding(BORDER_LIGHT, Color::rgb8(0xa1, 0xa1, 0xa1))
         .adding(SELECTION_COLOR, Color::rgb8(0xf3, 0x00, 0x21))
         .adding(CURSOR_COLOR, Color::WHITE)
+
         .adding(TEXT_SIZE_NORMAL, 15.0)
         .adding(BASIC_WIDGET_HEIGHT, 18.0)
         .adding(BORDERED_WIDGET_HEIGHT, 24.0)
+
         .adding(SCROLL_BAR_COLOR, Color::rgb8(0xff, 0xff, 0xff))
-        .adding(SCROLL_BAR_BORDER_COLOR, Color::rgb8(0x77, 0x77, 0x77));
+        .adding(SCROLL_BAR_BORDER_COLOR, Color::rgb8(0x77, 0x77, 0x77))
+        .adding(SCROLL_BAR_WIDTH, 8.)
+        .adding(SCROLL_BAR_PAD, 2.);
 
     #[cfg(target_os = "windows")]
     {
