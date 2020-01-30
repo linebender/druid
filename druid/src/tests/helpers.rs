0dc61b38b3190269165302c01a274dee4c32da34
Colin Rofls <colin@cmyr.net>
2020-01-30 14:56:56 -0500

    Add test helpers and refactor harness
    This moves to having harness act a lot like a combination of
    AppState and a window, instead of just being a context factory.

---
+++ druid/src/tests/helpers.rs
@@ -0,0 +1,126 @@
+// Copyright 2020 The xi-editor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Helper types for test writing.
+//!
+//! This includes tools for making throwaway widgets more easily.
+
+use crate::*;
+
+pub type EventFn<S, T> = dyn FnMut(&mut S, &mut EventCtx, &Event, &T, &Env);
+pub type LifeCycleFn<S, T> = dyn FnMut(&mut S, &mut LifeCycleCtx, &LifeCycle, &T, &Env);
+pub type UpdateFn<S, T> = dyn FnMut(&mut S, &mut UpdateCtx, &T, &T, &Env);
+pub type LayoutFn<S, T> = dyn FnMut(&mut S, &mut LayoutCtx, &BoxConstraints, &T, &Env) -> Size;
+pub type PaintFn<S, T> = dyn FnMut(&mut S, &mut PaintCtx, &T, &Env);
+
+/// A widget that can be constructed from individual functions, builder-style.
+///
+/// This widget is generic over its state, which is passed in at construction time.
+pub struct ModularWidget<S, T> {
+    state: S,
+    event: Option<Box<EventFn<S, T>>>,
+    lifecycle: Option<Box<LifeCycleFn<S, T>>>,
+    update: Option<Box<UpdateFn<S, T>>>,
+    layout: Option<Box<LayoutFn<S, T>>>,
+    paint: Option<Box<PaintFn<S, T>>>,
+}
+
+#[allow(dead_code)]
+impl<S, T> ModularWidget<S, T> {
+    pub fn new(state: S) -> Self {
+        ModularWidget {
+            state,
+            event: None,
+            lifecycle: None,
+            update: None,
+            layout: None,
+            paint: None,
+        }
+    }
+
+    pub fn event_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut EventCtx, &Event, &T, &Env) + 'static,
+    ) -> Self {
+        self.event = Some(Box::new(f));
+        self
+    }
+
+    pub fn lifecycle_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut LifeCycleCtx, &LifeCycle, &T, &Env) + 'static,
+    ) -> Self {
+        self.lifecycle = Some(Box::new(f));
+        self
+    }
+
+    pub fn update_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut UpdateCtx, &T, &T, &Env) + 'static,
+    ) -> Self {
+        self.update = Some(Box::new(f));
+        self
+    }
+
+    pub fn layout_fn(
+        mut self,
+        f: impl FnMut(&mut S, &mut LayoutCtx, &BoxConstraints, &T, &Env) -> Size + 'static,
+    ) -> Self {
+        self.layout = Some(Box::new(f));
+        self
+    }
+
+    pub fn paint_fn(mut self, f: impl FnMut(&mut S, &mut PaintCtx, &T, &Env) + 'static) -> Self {
+        self.paint = Some(Box::new(f));
+        self
+    }
+}
+
+impl<S, T: Data> Widget<T> for ModularWidget<S, T> {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
+        if let Some(f) = self.event.as_mut() {
+            f(&mut self.state, ctx, event, data, env)
+        }
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
+        if let Some(f) = self.lifecycle.as_mut() {
+            f(&mut self.state, ctx, event, data, env)
+        }
+    }
+
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
+        if let Some(f) = self.update.as_mut() {
+            f(&mut self.state, ctx, old_data, data, env)
+        }
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &T, env: &Env) -> Size {
+        let ModularWidget {
+            ref mut state,
+            ref mut layout,
+            ..
+        } = self;
+        layout
+            .as_mut()
+            .map(|f| f(state, ctx, bc, data, env))
+            .unwrap_or(Size::new(100., 100.))
+    }
+
+    fn paint(&mut self, ctx: &mut PaintCtx, data: &T, env: &Env) {
+        if let Some(f) = self.paint.as_mut() {
+            f(&mut self.state, ctx, data, env)
+        }
+    }
+}
--- druid/src/tests/mod.rs
+++ druid/src/tests/mod.rs
@@ -15,3 +15,37 @@
 //! Additional unit tests that cross file or module boundaries.
 
 mod harness;
+mod helpers;
+
+use crate::widget::*;
+use crate::*;
+use harness::*;
+use helpers::*;
+
+/// test that the first widget to request focus during an event gets it.
+#[test]
+fn take_focus() {
+    const TAKE_FOCUS: Selector = Selector::new("druid-tests.take-focus");
+
+    /// A widget that takes focus when sent a particular command.
+    fn make_focus_taker() -> impl Widget<bool> {
+        ModularWidget::new(()).event_fn(|_, ctx, event, _data, _env| {
+            if let Event::Command(cmd) = event {
+                if cmd.selector == TAKE_FOCUS {
+                    ctx.request_focus();
+                }
+            }
+        })
+    }
+
+    let left_id = WidgetId::next();
+    let left = make_focus_taker().with_id(left_id);
+    let right = make_focus_taker();
+    let app = Split::vertical(left, right);
+    let data = true;
+
+    Harness::create(data, app, |harness| {
+        harness.event(Event::Command(TAKE_FOCUS.into()));
+        assert_eq!(harness.window.focus, Some(left_id));
+    })
+}
