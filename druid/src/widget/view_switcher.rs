c450d2d2e852a59014bbf2036fce5df36fc742f0
Ankur Sethi <contact@ankursethi.in>
2020-02-06 15:31:39 +0530

    Add license headers and documentation

--- druid/src/widget/view_switcher.rs
+++ druid/src/widget/view_switcher.rs
@@ -1,8 +1,26 @@
+// Copyright 2019 The xi-editor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A widget that can dynamically switch between one of many views.
+
 use crate::{
     BoxConstraints, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, PaintCtx,
     Point, Rect, Size, UpdateCtx, Widget, WidgetPod,
 };
 
+/// A widget that can switch dynamically between one of many views depending
+/// application state.
 pub struct ViewSwitcher<T: Data, U: PartialEq + Clone> {
     child_selector: Box<dyn Fn(&T, &Env) -> U>,
     child_builder: Box<dyn Fn(U, &Env) -> Box<dyn Widget<T>>>,
@@ -11,6 +29,15 @@ pub struct ViewSwitcher<T: Data, U: PartialEq + Clone> {
 }
 
 impl<T: Data, U: PartialEq + Clone> ViewSwitcher<T, U> {
+    /// Create a new view switcher.
+    ///
+    /// The `child_selector` closure is called every time the application data changes.
+    /// If the value it returns is the same as the one it returned during the previous
+    /// data change, nothing happens. If it returns a different value, then the
+    /// `child_builder` closure is called with the new value.
+    ///
+    /// The `child_builder` closure is expected to create a new child widget depending
+    /// on the selector to it.
     pub fn new(
         child_selector: impl Fn(&T, &Env) -> U + 'static,
         child_builder: impl Fn(U, &Env) -> Box<dyn Widget<T>> + 'static,
@@ -47,7 +74,8 @@ impl<T: Data, U: PartialEq + Clone> Widget<T> for ViewSwitcher<T, U> {
         match &self.active_child_id {
             Some(current_child_id) => {
                 if &child_id != current_child_id {
-                    self.active_child = Some(WidgetPod::new((self.child_builder)(child_id.clone(), env)));
+                    self.active_child =
+                        Some(WidgetPod::new((self.child_builder)(child_id.clone(), env)));
                     self.active_child_id = Some(child_id);
                     ctx.children_changed();
                 }
