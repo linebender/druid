6dcbd1b026b42d1317d946b8457b5451c0b9db14
Ankur Sethi <contact@ankursethi.in>
2020-02-06 15:38:26 +0530

    Minor cleanup

--- druid/src/widget/view_switcher.rs
+++ druid/src/widget/view_switcher.rs
@@ -21,14 +21,14 @@ use crate::{
 
 /// A widget that can switch dynamically between one of many views depending
 /// application state.
-pub struct ViewSwitcher<T: Data, U: PartialEq + Clone> {
+pub struct ViewSwitcher<T: Data, U: PartialEq> {
     child_selector: Box<dyn Fn(&T, &Env) -> U>,
-    child_builder: Box<dyn Fn(U, &Env) -> Box<dyn Widget<T>>>,
+    child_builder: Box<dyn Fn(&U, &Env) -> Box<dyn Widget<T>>>,
     active_child: Option<WidgetPod<T, Box<dyn Widget<T>>>>,
     active_child_id: Option<U>,
 }
 
-impl<T: Data, U: PartialEq + Clone> ViewSwitcher<T, U> {
+impl<T: Data, U: PartialEq> ViewSwitcher<T, U> {
     /// Create a new view switcher.
     ///
     /// The `child_selector` closure is called every time the application data changes.
@@ -40,7 +40,7 @@ impl<T: Data, U: PartialEq + Clone> ViewSwitcher<T, U> {
     /// on the selector to it.
     pub fn new(
         child_selector: impl Fn(&T, &Env) -> U + 'static,
-        child_builder: impl Fn(U, &Env) -> Box<dyn Widget<T>> + 'static,
+        child_builder: impl Fn(&U, &Env) -> Box<dyn Widget<T>> + 'static,
     ) -> Self {
         Self {
             child_selector: Box::new(child_selector),
@@ -51,7 +51,7 @@ impl<T: Data, U: PartialEq + Clone> ViewSwitcher<T, U> {
     }
 }
 
-impl<T: Data, U: PartialEq + Clone> Widget<T> for ViewSwitcher<T, U> {
+impl<T: Data, U: PartialEq> Widget<T> for ViewSwitcher<T, U> {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
         if let Some(ref mut child) = self.active_child {
             child.event(ctx, event, data, env);
@@ -61,8 +61,8 @@ impl<T: Data, U: PartialEq + Clone> Widget<T> for ViewSwitcher<T, U> {
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
         if let LifeCycle::WidgetAdded = event {
             let child_id = (self.child_selector)(data, env);
-            self.active_child_id = Some(child_id.clone());
-            self.active_child = Some(WidgetPod::new((self.child_builder)(child_id, env)));
+            self.active_child = Some(WidgetPod::new((self.child_builder)(&child_id, env)));
+            self.active_child_id = Some(child_id);
         }
         if let Some(ref mut child) = self.active_child {
             child.lifecycle(ctx, event, data, env);
@@ -71,16 +71,12 @@ impl<T: Data, U: PartialEq + Clone> Widget<T> for ViewSwitcher<T, U> {
 
     fn update(&mut self, ctx: &mut UpdateCtx, old_data: &T, data: &T, env: &Env) {
         let child_id = (self.child_selector)(data, env);
-        match &self.active_child_id {
-            Some(current_child_id) => {
-                if &child_id != current_child_id {
-                    self.active_child =
-                        Some(WidgetPod::new((self.child_builder)(child_id.clone(), env)));
-                    self.active_child_id = Some(child_id);
-                    ctx.children_changed();
-                }
+        if let Some(ref active_child_id) = self.active_child_id {
+            if &child_id != active_child_id {
+                self.active_child = Some(WidgetPod::new((self.child_builder)(&child_id, env)));
+                self.active_child_id = Some(child_id);
+                ctx.children_changed();
             }
-            None => {}
         }
 
         if !old_data.same(data) {
