c0af6a95eae48353e97765a0e53e3fbc8e84e93a
Kaiyin Zhong <kindlychung@gmai.com>
2020-03-03 22:22:42 +0100

    refactor, fix warnings

--- druid/examples/game_of_life.rs
+++ druid/examples/game_of_life.rs
@@ -14,62 +14,91 @@
 
 //! Game of life
 
-use druid::kurbo::BezPath;
-use druid::piet::{FontBuilder, ImageFormat, InterpolationMode, Text, TextLayoutBuilder};
+use std::ops::{Index, IndexMut};
+use std::time::{Duration, Instant};
 
+use druid::{AppLauncher, BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, LocalizedString, PaintCtx, Point, Rect, RenderContext, Size, TimerToken, UpdateCtx, Widget, WindowDesc};
 
-use druid::{Affine, AppLauncher, BoxConstraints, Color, Env, Event, EventCtx, LayoutCtx, LifeCycle, LifeCycleCtx, LocalizedString, PaintCtx, Point, Rect, RenderContext, Size, UpdateCtx, Widget, WindowDesc, Data, TimerToken};
-use std::time::{Instant, Duration};
-
-
-const POOL_SIZE: i32 = 1600;
+const GRID_SIZE: usize = 40;
+const POOL_SIZE: usize = GRID_SIZE * GRID_SIZE;
 const CELL_COLOR: Color = Color::rgb8(0xf3 as u8, 0xf4 as u8, 8 as u8);
 
 #[derive(Clone)]
-struct AppData {
-    grid: [i32; 1600],
+struct Grid {
+    storage: [bool; POOL_SIZE],
 }
 
-impl AppData {
-    pub fn evolve(&mut self) {
-        let mut indices_to_mutate: Vec<usize> = vec![];
-        for i in 0..POOL_SIZE {
-            let neighbors = AppData::neighbors(i);
-            let mut sum = 0;
-            for nb_i in &neighbors {
-                if *nb_i >= 0 && *nb_i < POOL_SIZE {
-                    let idx = *nb_i as usize;
-                    let neighbor_life = self.grid[idx];
-                    if neighbor_life > 0 {
-                        sum += neighbor_life;
-                    }
-                }
-            }
-            let life = self.grid[i as usize];
-            // death by loneliness or overcrowding
-            if life == 1 && (sum < 2 || sum > 3) {
-                indices_to_mutate.push(i as usize);
-                continue;
-            }
-            if life == -1 && sum == 3 {
-                indices_to_mutate.push(i as usize);
-            }
-        }
-        for i_mut in indices_to_mutate {
-            self.grid[i_mut] *= -1;
+#[derive(Clone, Copy, PartialEq)]
+struct GridPos {
+    row: usize,
+    col: usize,
+}
+
+impl GridPos {
+    pub fn above(&self) -> Option<GridPos> {
+        if self.row == 0 {
+            None
+        } else {
+            Some(GridPos { row: self.row - 1, col: self.col })
         }
     }
-
-    // return the indices of neighbors of a cell, clockwise, starting from top-left corner
-    pub fn neighbors(i: i32) -> [i32; 8] {
-        return [i - 41, i - 40, i - 39, i + 1, i + 41, i + 40, i + 39, i - 1];
+    pub fn below(&self) -> Option<GridPos> {
+        if self.row == GRID_SIZE - 1 {
+            None
+        } else {
+            Some(GridPos { row: self.row + 1, col: self.col })
+        }
+    }
+    pub fn left(&self) -> Option<GridPos> {
+        if self.col == 0 {
+            None
+        } else {
+            Some(GridPos { row: self.row, col: self.col - 1 })
+        }
+    }
+    pub fn right(&self) -> Option<GridPos> {
+        if self.col == GRID_SIZE - 1 {
+            None
+        } else {
+            Some(GridPos { row: self.row, col: self.col + 1 })
+        }
+    }
+    #[allow(dead_code)]
+    pub fn above_left(&self) -> Option<GridPos> {
+        self.above().and_then(|pos| pos.left())
+    }
+    pub fn above_right(&self) -> Option<GridPos> {
+        self.above().and_then(|pos| pos.right())
+    }
+    #[allow(dead_code)]
+    pub fn below_left(&self) -> Option<GridPos> {
+        self.below().and_then(|pos| pos.left())
+    }
+    pub fn below_right(&self) -> Option<GridPos> {
+        self.below().and_then(|pos| pos.right())
     }
 }
 
-impl Data for AppData {
-    fn same(&self, other: &Self) -> bool {
+impl Index<GridPos> for Grid {
+    type Output = bool;
+
+    fn index(&self, pos: GridPos) -> &Self::Output {
+        let idx = pos.row * GRID_SIZE + pos.col;
+        self.storage.index(idx)
+    }
+}
+
+impl IndexMut<GridPos> for Grid {
+    fn index_mut(&mut self, pos: GridPos) -> &mut Self::Output {
+        let idx = pos.row * GRID_SIZE + pos.col;
+        self.storage.index_mut(idx)
+    }
+}
+
+impl PartialEq for Grid {
+    fn eq(&self, other: &Self) -> bool {
         for i in 0..POOL_SIZE {
-            if self.grid[i as usize] != other.grid[i as usize] {
+            if self.storage[i as usize] != other.storage[i as usize] {
                 return false;
             }
         }
@@ -77,25 +106,97 @@ impl Data for AppData {
     }
 }
 
-struct CustomWidget {
+#[derive(Clone)]
+struct AppData {
+    grid: Grid,
+}
+
+impl Grid {
+    pub fn new() -> Grid {
+        Grid { storage: [false; POOL_SIZE] }
+    }
+    pub fn evolve(&mut self) {
+        let mut indices_to_mutate: Vec<GridPos> = vec![];
+        for row in 0..GRID_SIZE {
+            for col in 0..GRID_SIZE {
+                let pos = GridPos { row, col };
+                let n_lives_around = self.n_neighbors(pos);
+                let life = self[pos];
+                // death by loneliness or overcrowding
+                if life && (n_lives_around < 2 || n_lives_around > 3) {
+                    indices_to_mutate.push(pos);
+                    continue;
+                }
+                // resurrection by life support
+                if !life && n_lives_around == 3 {
+                    indices_to_mutate.push(pos);
+                }
+            }
+        }
+        for pos_mut in indices_to_mutate {
+            self[pos_mut] = !self[pos_mut];
+        }
+    }
+
+    pub fn neighbors(pos: GridPos) -> [Option<GridPos>; 8] {
+        let above = pos.above();
+        let below = pos.below();
+        let left = pos.left();
+        let right = pos.right();
+        let above_left = above.and_then(|pos| pos.left());
+        let above_right = above.and_then(|pos| pos.right());
+        let below_left = below.and_then(|pos| pos.left());
+        let below_right = below.and_then(|pos| pos.right());
+        [above, below, left, right, above_left, above_right, below_left, below_right]
+    }
+
+    pub fn n_neighbors(&self, pos: GridPos) -> usize {
+        Grid::neighbors(pos)
+            .iter()
+            .filter(|x| {
+                x.is_some() && self[x.unwrap()]
+            }).count()
+    }
+
+    pub fn set_alive(&mut self, positions: &[GridPos]) {
+        for pos in positions {
+            self[*pos] = true;
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn set_dead(&mut self, positions: &[GridPos]) {
+        for pos in positions {
+            self[*pos] = false;
+        }
+    }
+}
+
+impl Data for AppData {
+    fn same(&self, other: &Self) -> bool {
+        self.grid == other.grid
+    }
+}
+
+struct GameOfLifeWidget {
     timer_id: TimerToken,
 }
 
 
-impl Widget<AppData> for CustomWidget {
-    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut AppData, _env: &Env) {
-        match _event {
+impl Widget<AppData> for GameOfLifeWidget {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppData, _env: &Env) {
+        match event {
             Event::WindowConnected => {
-                _ctx.request_paint();
-                let deadline = Instant::now() + Duration::from_millis(50);
-                self.timer_id = _ctx.request_timer(deadline);
+                ctx.request_paint();
+                let deadline = Instant::now() + Duration::from_millis(150);
+                self.timer_id = ctx.request_timer(deadline);
             }
             Event::Timer(id) => {
                 if *id == self.timer_id {
-                    _data.evolve();
-                    _ctx.request_paint();
-                    let deadline = Instant::now() + Duration::from_millis(500);
-                    self.timer_id = _ctx.request_timer(deadline);
+                    data.grid.evolve();
+                    ctx.request_paint();
+                    let deadline = Instant::now() + Duration::from_millis(150);
+                    self.timer_id = ctx.request_timer(deadline);
                 }
             }
             _ => {}
@@ -127,53 +228,57 @@ impl Widget<AppData> for CustomWidget {
         let w0 = size.width / 40.0;
         let h0 = size.height / 40.0;
         let cell_size = Size { width: w0, height: h0 };
-        for i in 0..1600 {
-            if data.grid[i] == 1 {
-                let xi = i % 40;
-                let yi = i / 40;
-                let pos = Point { x: w0 * xi as f64, y: h0 * yi as f64 };
-                let rect = Rect::from_origin_size(pos, cell_size);
-                paint_ctx.fill(rect, &CELL_COLOR);
+        for row in 0..GRID_SIZE {
+            for col in 0..GRID_SIZE {
+                let pos = GridPos { row, col };
+                if data.grid[pos] {
+                    let pos = Point { x: w0 * row as f64, y: h0 * col as f64 };
+                    let rect = Rect::from_origin_size(pos, cell_size);
+                    paint_ctx.fill(rect, &CELL_COLOR);
+                }
             }
         }
     }
 }
 
+// gives back positions of a glider pattern
+//   *
+// * *
+//  **
+fn glider(left_most: GridPos) -> Option<[GridPos; 5]> {
+    if left_most.row < 1 || left_most.row > GRID_SIZE - 2 || left_most.col > GRID_SIZE - 3 {
+        return None;
+    }
+    let center = left_most.right().unwrap();
+    Some([left_most, center.below_right().unwrap(), center.below().unwrap(), center.right().unwrap(), center.above_right().unwrap()])
+}
+
+
+// gives back positions of a blinker pattern
+//  *
+//  *
+//  *
+fn blinker(top: GridPos) -> Option<[GridPos; 3]> {
+    if top.row > GRID_SIZE - 3 || top.col < 1 || top.col > GRID_SIZE - 2 {
+        return None;
+    }
+    let center = top.below().unwrap();
+    Some([top, center, center.below().unwrap()])
+}
+
 fn main() {
-    let window = WindowDesc::new(|| CustomWidget {timer_id: TimerToken::INVALID}).title(
+    let window = WindowDesc::new(|| GameOfLifeWidget { timer_id: TimerToken::INVALID }).title(
         LocalizedString::new("custom-widget-demo-window-title").with_placeholder("Game of Life"),
     );
-    let mut grid = [-1; 1600];
-    // glider
-    grid[40] = 1;
-    grid[81] = 1;
-    grid[82] = 1;
-    grid[42] = 1;
-    grid[2] = 1;
-    // blinker
-    grid[54] = 1;
-    grid[55] = 1;
-    grid[56] = 1;
-    // toad
-    grid[375] = 1;
-    grid[376] = 1;
-    grid[377] = 1;
-    grid[416] = 1;
-    grid[417] = 1;
-    grid[418] = 1;
-    // penta-decathlon
-    grid[72] = 1;
-    grid[112] = 1;
-    grid[192] = 1;
-    grid[232] = 1;
-    grid[272] = 1;
-    grid[312] = 1;
-    grid[392] = 1;
-    grid[432] = 1;
-    grid[151] = 1;
-    grid[153] = 1;
-    grid[351] = 1;
-    grid[353] = 1;
+    let mut grid = Grid::new();
+    let pattern0 = glider(GridPos { row: 5, col: 5 });
+    for x in &pattern0 {
+        grid.set_alive(x);
+    }
+    let pattern1 = blinker(GridPos { row: 29, col: 29 });
+    for x in &pattern1 {
+        grid.set_alive(x);
+    }
     AppLauncher::with_window(window)
         .use_simple_logger()
         .launch(AppData { grid })
