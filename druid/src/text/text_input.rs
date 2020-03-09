7824ca1739774d4257764831a3fb86bc899503b9
pyroxymat <pyroxymat@tutanota.com>
2020-03-09 23:00:45 +0100

    Move EventDomain into EditAction

--- druid/src/text/text_input.rs
+++ druid/src/text/text_input.rs
@@ -9,32 +9,21 @@ use druid_shell::KeyEvent;
 
 /// Events that only modify view state
 #[derive(Debug, PartialEq, Clone)]
-pub enum ViewEvent {
+pub enum EditAction {
     Move(Movement),
     ModifySelection(Movement),
     SelectAll,
     // Click(MouseAction),
     // Drag(MouseAction),
-}
-
-/// Events that modify the buffer
-#[derive(Debug, PartialEq, Clone)]
-pub enum BufferEvent {
     Delete, // { movement: Movement, kill: bool },
     Backspace,
     Insert(String),
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub enum EventDomain {
-    View(ViewEvent),
-    Buffer(BufferEvent),
-}
-
 pub trait TextInput {
     /// Handle a key event and return an edit action to be executed
     /// for the key event
-    fn handle_event(&self, event: &KeyEvent) -> Option<EventDomain>;
+    fn handle_event(&self, event: &KeyEvent) -> Option<EditAction>;
 }
 
 /// Handles key events and returns actions that are applicable to
@@ -48,52 +37,46 @@ impl SingleLineTextInput {
 }
 
 impl TextInput for SingleLineTextInput {
-    fn handle_event(&self, event: &KeyEvent) -> Option<EventDomain> {
+    fn handle_event(&self, event: &KeyEvent) -> Option<EditAction> {
         let action = match event {
             // Select all (Ctrl+A || Cmd+A)
-            k_e if (HotKey::new(SysMods::Cmd, "a")).matches(k_e) => {
-                EventDomain::View(ViewEvent::SelectAll)
-            }
+            k_e if (HotKey::new(SysMods::Cmd, "a")).matches(k_e) => EditAction::SelectAll,
             // Jump left (Ctrl+ArrowLeft || Cmd+ArrowLeft)
             k_e if (HotKey::new(SysMods::Cmd, KeyCode::ArrowLeft)).matches(k_e)
                 || HotKey::new(None, KeyCode::Home).matches(k_e) =>
             {
-                EventDomain::View(ViewEvent::Move(Movement::LeftOfLine))
+                EditAction::Move(Movement::LeftOfLine)
             }
             // Jump right (Ctrl+ArrowRight || Cmd+ArrowRight)
             k_e if (HotKey::new(SysMods::Cmd, KeyCode::ArrowRight)).matches(k_e)
                 || HotKey::new(None, KeyCode::End).matches(k_e) =>
             {
-                EventDomain::View(ViewEvent::Move(Movement::RightOfLine))
+                EditAction::Move(Movement::RightOfLine)
             }
             // Select left (Shift+ArrowLeft)
             k_e if (HotKey::new(SysMods::Shift, KeyCode::ArrowLeft)).matches(k_e) => {
-                EventDomain::View(ViewEvent::ModifySelection(Movement::Left))
+                EditAction::ModifySelection(Movement::Left)
             }
             // Select right (Shift+ArrowRight)
             k_e if (HotKey::new(SysMods::Shift, KeyCode::ArrowRight)).matches(k_e) => {
-                EventDomain::View(ViewEvent::ModifySelection(Movement::Right))
+                EditAction::ModifySelection(Movement::Right)
             }
             // Move left (ArrowLeft)
             k_e if (HotKey::new(None, KeyCode::ArrowLeft)).matches(k_e) => {
-                EventDomain::View(ViewEvent::Move(Movement::Left))
+                EditAction::Move(Movement::Left)
             }
             // Move right (ArrowRight)
             k_e if (HotKey::new(None, KeyCode::ArrowRight)).matches(k_e) => {
-                EventDomain::View(ViewEvent::Move(Movement::Right))
+                EditAction::Move(Movement::Right)
             }
             // Backspace
-            k_e if (HotKey::new(None, KeyCode::Backspace)).matches(k_e) => {
-                EventDomain::Buffer(BufferEvent::Backspace)
-            }
+            k_e if (HotKey::new(None, KeyCode::Backspace)).matches(k_e) => EditAction::Backspace,
             // Delete
-            k_e if (HotKey::new(None, KeyCode::Delete)).matches(k_e) => {
-                EventDomain::Buffer(BufferEvent::Delete)
-            }
+            k_e if (HotKey::new(None, KeyCode::Delete)).matches(k_e) => EditAction::Delete,
             // Actual typing
             k_e if k_e.key_code.is_printable() => {
                 if let Some(chars) = k_e.text() {
-                    EventDomain::Buffer(BufferEvent::Insert(chars.to_owned()))
+                    EditAction::Insert(chars.to_owned())
                 } else {
                     return None;
                 }
