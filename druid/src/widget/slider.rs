a0c243552b9f778bdae1cf04ce38cca3eacd4f6c
Kaiyin Zhong <kindlychung@gmai.com>
2020-03-12 19:51:05 +0100

    fmt

--- druid/src/widget/slider.rs
+++ druid/src/widget/slider.rs
@@ -92,7 +92,7 @@ impl Widget<f64> for Slider {
         }
     }
 
-    fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &f64, _env: &Env) { }
+    fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &f64, _env: &Env) {}
 
     fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &f64, _data: &f64, _env: &Env) {
         ctx.request_paint();
