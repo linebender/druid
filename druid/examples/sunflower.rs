9bc6ada759081654507a694778a80a1dc8bbdafa
Kaiyin Zhong <kindlychung@gmai.com>
2020-03-13 00:01:04 +0100

    fmt

--- druid/examples/sunflower.rs
+++ druid/examples/sunflower.rs
@@ -18,13 +18,13 @@ use std::f64::consts::{E, PI, SQRT_2};
 use std::ops::{Index, IndexMut};
 use std::time::{Duration, Instant};
 
-use druid::widget::{Button, CrossAxisAlignment, Flex, Label, Slider, Stepper, WidgetExt, TextBox};
-use druid::{
-    AppLauncher, BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, LensExt, Lens, LifeCycle,
-    LifeCycleCtx, LocalizedString, MouseButton, PaintCtx, Point, Rect, RenderContext, Size,
-    TimerToken, UpdateCtx, Widget, WindowDesc,
-};
 use druid::kurbo::Circle;
+use druid::widget::{Button, CrossAxisAlignment, Flex, Label, Slider, Stepper, TextBox, WidgetExt};
+use druid::{
+    AppLauncher, BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, Lens, LensExt,
+    LifeCycle, LifeCycleCtx, LocalizedString, MouseButton, PaintCtx, Point, Rect, RenderContext,
+    Size, TimerToken, UpdateCtx, Widget, WindowDesc,
+};
 use fluent_syntax::ast::InlineExpression::StringLiteral;
 
 const e_inv: f64 = 1. / E;
@@ -52,7 +52,8 @@ impl Widget<AppData> for SunflowerWidget {
         _event: &LifeCycle,
         _data: &AppData,
         _env: &Env,
-    ) {}
+    ) {
+    }
 
     fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &AppData, _data: &AppData, _env: &Env) {
         ctx.request_paint();
@@ -76,16 +77,16 @@ impl Widget<AppData> for SunflowerWidget {
     fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &AppData, _env: &Env) {
         let size: Size = paint_ctx.size();
         let m = size.width as f64 / 2.;
-        let center = Point {
-            x: m,
-            y: m,
-        };
+        let center = Point { x: m, y: m };
         for i in 0..data.n_seeds as u64 {
             let theta = i as f64 * 2. * PI * data.angle_factor;
             let r = (i as f64).sqrt() * data.scale;
             let x = m + r * theta.cos();
             let y = m - r * theta.sin();
-            paint_ctx.fill(Circle::new(Point { x, y }, 3.), &Color::rgb8(233 as u8, 233 as u8, 0 as u8));
+            paint_ctx.fill(
+                Circle::new(Point { x, y }, 3.),
+                &Color::rgb8(233 as u8, 233 as u8, 0 as u8),
+            );
         }
     }
 }
@@ -99,7 +100,7 @@ fn make_widget() -> impl Widget<AppData> {
                     Label::new(|data: &AppData, _env: &_| {
                         format!("Angle incrementation factor: {:.8}", data.angle_factor)
                     })
-                        .padding(3.0),
+                    .padding(3.0),
                     0.,
                 )
                 .with_child(
@@ -108,32 +109,32 @@ fn make_widget() -> impl Widget<AppData> {
                             Button::new("Use SQRT2", |ctx, data: &mut f64, _: &Env| {
                                 *data = two_sqrt_inv;
                             })
-                                .lens(AppData::angle_factor)
-                                .padding((5., 5.)),
+                            .lens(AppData::angle_factor)
+                            .padding((5., 5.)),
                             1.0,
                         )
                         .with_child(
                             Button::new("Use SQRT3", |ctx, data: &mut f64, _: &Env| {
                                 *data = three_sqrt_inv;
                             })
-                                .lens(AppData::angle_factor)
-                                .padding((5., 5.)),
+                            .lens(AppData::angle_factor)
+                            .padding((5., 5.)),
                             1.0,
                         )
                         .with_child(
                             Button::new("Use e", |ctx, data: &mut f64, _: &Env| {
                                 *data = e_inv;
                             })
-                                .lens(AppData::angle_factor)
-                                .padding((5., 5.)),
+                            .lens(AppData::angle_factor)
+                            .padding((5., 5.)),
                             1.0,
                         )
                         .with_child(
                             Button::new("Use golden ratio", |ctx, data: &mut f64, _: &Env| {
                                 *data = phi_inv;
                             })
-                                .lens(AppData::angle_factor)
-                                .padding((5., 5.)),
+                            .lens(AppData::angle_factor)
+                            .padding((5., 5.)),
                             1.0,
                         )
                         .padding(8.0),
@@ -165,36 +166,34 @@ fn make_widget() -> impl Widget<AppData> {
                     Label::new(|data: &AppData, _env: &_| {
                         format!("Number of seeds: {}", data.n_seeds)
                     })
-                        .padding(3.0),
+                    .padding(3.0),
                     0.,
                 )
                 .with_child(
-                    Flex::row()
-                        .with_child(
-                            Slider::new()
-                                .with_range(10., 5000.0)
-                                .lens(AppData::n_seeds)
-                                .padding(3.),
-                            1.,
-                        ),
+                    Flex::row().with_child(
+                        Slider::new()
+                            .with_range(10., 5000.0)
+                            .lens(AppData::n_seeds)
+                            .padding(3.),
+                        1.,
+                    ),
                     0.,
                 )
                 .with_child(
                     Label::new(|data: &AppData, _env: &_| {
                         format!("Radius increment scale {}", data.scale)
                     })
-                        .padding(3.0),
+                    .padding(3.0),
                     0.,
                 )
                 .with_child(
-                    Flex::row()
-                        .with_child(
-                            Slider::new()
-                                .with_range(4., 10.)
-                                .lens(AppData::scale)
-                                .padding(3.),
-                            1.,
-                        ),
+                    Flex::row().with_child(
+                        Slider::new()
+                            .with_range(4., 10.)
+                            .lens(AppData::scale)
+                            .padding(3.),
+                        1.,
+                    ),
                     0.,
                 )
                 .cross_axis_alignment(CrossAxisAlignment::Center)
@@ -213,8 +212,7 @@ fn main() {
         })
         .resizable(false)
         .title(
-            LocalizedString::new("custom-widget-demo-window-title")
-                .with_placeholder("Sunflower"),
+            LocalizedString::new("custom-widget-demo-window-title").with_placeholder("Sunflower"),
         );
     AppLauncher::with_window(window)
         .use_simple_logger()
