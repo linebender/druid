3a6ce2332a5d3c0ac74ee28b0a1ef51bb29971ef
Kaiyin Zhong <kindlychung@gmai.com>
2020-03-13 17:54:40 +0100

    remove unused imports and vars

--- druid/examples/sunflower.rs
+++ druid/examples/sunflower.rs
@@ -1,4 +1,5 @@
 // Copyright 2019 The xi-editor Authors.
+
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,26 +16,22 @@
 //! Sunflower
 
 use std::f64::consts::{E, PI, SQRT_2};
-use std::ops::{Index, IndexMut};
-use std::time::{Duration, Instant};
 
 use druid::kurbo::Circle;
-use druid::widget::{Button, CrossAxisAlignment, Flex, Label, Slider, Stepper, TextBox, WidgetExt};
+use druid::widget::{Button, CrossAxisAlignment, Flex, Label, Stepper, TextBox, WidgetExt};
 use druid::{
     AppLauncher, BoxConstraints, Color, Data, Env, Event, EventCtx, LayoutCtx, Lens, LensExt,
-    LifeCycle, LifeCycleCtx, LocalizedString, MouseButton, PaintCtx, Point, Rect, RenderContext,
-    Size, TimerToken, UpdateCtx, Widget, WindowDesc,
+    LifeCycle, LifeCycleCtx, LocalizedString, PaintCtx, Point, RenderContext, Size, UpdateCtx,
+    Widget, WindowDesc,
 };
-use fluent_syntax::ast::InlineExpression::StringLiteral;
 
-const e_inv: f64 = 1. / E;
-const phi_inv: f64 = 1. / 1.61803398875;
-const two_sqrt_inv: f64 = 1. / SQRT_2;
-const three_sqrt_inv: f64 = 1. / 1.73205080757;
-const pi_inv: f64 = 1. / PI;
-const default_angle_factor: f64 = 0.256666666666;
-const default_n_seeds: f64 = 1000.;
-const default_radius_scale: f64 = 6.;
+const E_INV: f64 = 1. / E;
+const PHI_INV: f64 = 1. / 1.61803398875;
+const TWO_SQRT_INV: f64 = 1. / SQRT_2;
+const THREE_SQRT_INV: f64 = 1. / 1.73205080757;
+const DEFAULT_ANGLE_FACTOR: f64 = 0.256666666666;
+const DEFAULT_N_SEEDS: f64 = 1000.;
+const DEFAULT_RADIUS_SCALE: f64 = 6.;
 
 #[derive(Clone, Lens, Data)]
 struct AppData {
@@ -46,7 +43,7 @@ struct AppData {
 struct SunflowerWidget {}
 
 impl Widget<AppData> for SunflowerWidget {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut AppData, _env: &Env) {}
+    fn event(&mut self, _ctx: &mut EventCtx,_eventt: &Event, _data: &mut AppData, _env: &Env) {}
 
     fn lifecycle(
         &mut self,
@@ -79,7 +76,6 @@ impl Widget<AppData> for SunflowerWidget {
     fn paint(&mut self, paint_ctx: &mut PaintCtx, data: &AppData, _env: &Env) {
         let size: Size = paint_ctx.size();
         let m = size.width as f64 / 2.;
-        let center = Point { x: m, y: m };
         for i in 0..data.n_seeds as u64 {
             let theta = i as f64 * 2. * PI * data.angle_factor;
             let r = (i as f64).sqrt() * data.scale;
@@ -104,8 +100,8 @@ fn make_widget() -> impl Widget<AppData> {
                         .with_child(Label::new("Angle factor shortcuts").padding(10.), 0.)
                         .with_child(
                             Flex::row().with_child(
-                                Button::new("Use SQRT2", |ctx, data: &mut f64, _: &Env| {
-                                    *data = two_sqrt_inv;
+                                Button::new("Use SQRT2", |_ctx, data: &mut f64, _: &Env| {
+                                    *data = TWO_SQRT_INV;
                                 })
                                 .lens(AppData::angle_factor)
                                 .padding((5., 5.)),
@@ -115,8 +111,8 @@ fn make_widget() -> impl Widget<AppData> {
                         )
                         .with_child(
                             Flex::row().with_child(
-                                Button::new("Use SQRT3", |ctx, data: &mut f64, _: &Env| {
-                                    *data = three_sqrt_inv;
+                                Button::new("Use SQRT3", |_ctx, data: &mut f64, _: &Env| {
+                                    *data = THREE_SQRT_INV;
                                 })
                                 .lens(AppData::angle_factor)
                                 .padding((5., 5.)),
@@ -126,8 +122,8 @@ fn make_widget() -> impl Widget<AppData> {
                         )
                         .with_child(
                             Flex::row().with_child(
-                                Button::new("Use e", |ctx, data: &mut f64, _: &Env| {
-                                    *data = e_inv;
+                                Button::new("Use e", |_ctx, data: &mut f64, _: &Env| {
+                                    *data = E_INV;
                                 })
                                 .lens(AppData::angle_factor)
                                 .padding((5., 5.)),
@@ -137,8 +133,8 @@ fn make_widget() -> impl Widget<AppData> {
                         )
                         .with_child(
                             Flex::row().with_child(
-                                Button::new("Use golden ratio", |ctx, data: &mut f64, _: &Env| {
-                                    *data = phi_inv;
+                                Button::new("Use golden ratio", |_ctx, data: &mut f64, _: &Env| {
+                                    *data = PHI_INV;
                                 })
                                 .lens(AppData::angle_factor)
                                 .padding((5., 5.)),
@@ -159,11 +155,11 @@ fn make_widget() -> impl Widget<AppData> {
                                 .with_child(Label::new("Angle factor"), 1.)
                                 .with_child(
                                     TextBox::new()
-                                        .with_placeholder(default_angle_factor.to_string())
+                                        .with_placeholder(DEFAULT_ANGLE_FACTOR.to_string())
                                         .parse()
                                         .lens(AppData::angle_factor.map(
                                             |x| Some(*x),
-                                            |x, y| *x = y.unwrap_or(default_angle_factor),
+                                            |x, y| *x = y.unwrap_or(DEFAULT_ANGLE_FACTOR),
                                         )),
                                     2.0,
                                 )
@@ -183,11 +179,11 @@ fn make_widget() -> impl Widget<AppData> {
                                 .with_child(Label::new("Seed number"), 1.)
                                 .with_child(
                                     TextBox::new()
-                                        .with_placeholder(default_n_seeds.to_string())
+                                        .with_placeholder(DEFAULT_N_SEEDS.to_string())
                                         .parse()
                                         .lens(AppData::n_seeds.map(
                                             |x| Some(*x),
-                                            |x, y| *x = y.unwrap_or(default_n_seeds),
+                                            |x, y| *x = y.unwrap_or(DEFAULT_N_SEEDS),
                                         )),
                                     2.0,
                                 )
@@ -207,11 +203,11 @@ fn make_widget() -> impl Widget<AppData> {
                                 .with_child(Label::new("Radius scale"), 1.)
                                 .with_child(
                                     TextBox::new()
-                                        .with_placeholder(default_radius_scale.to_string())
+                                        .with_placeholder(DEFAULT_RADIUS_SCALE.to_string())
                                         .parse()
                                         .lens(AppData::scale.map(
                                             |x| Some(*x),
-                                            |x, y| *x = y.unwrap_or(default_radius_scale),
+                                            |x, y| *x = y.unwrap_or(DEFAULT_RADIUS_SCALE),
                                         )),
                                     2.0,
                                 )
@@ -248,7 +244,7 @@ fn main() {
     AppLauncher::with_window(window)
         .use_simple_logger()
         .launch(AppData {
-            angle_factor: default_angle_factor,
+            angle_factor: DEFAULT_ANGLE_FACTOR,
             n_seeds: 600.,
             scale: 4.0,
         })
