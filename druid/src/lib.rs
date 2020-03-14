38418ce6f89bf1fa6018fc2820611d0753b021f5
Colin Rofls <colin@cmyr.net>
2020-03-14 14:45:20 -0400

    Update main crate docs

--- druid/src/lib.rs
+++ druid/src/lib.rs
@@ -13,9 +13,95 @@
 // limitations under the License.
 
 //! Simple data-oriented GUI.
+//!
+//! Druid lets you build simple interactive graphical applications that
+//! can be deployed on windows, mac, and linux.
+//!
+//! Druid is built on top of [`druid-shell`], which implements all of the
+//! lower-level, platform-specific code, providing a common abstraction
+//! for things like key and mouse events, creating windows, and launching
+//! an application. Below [`druid-shell`] is [`piet`], which is a cross-platform
+//! 2D graphics library, providing a simple and familiar drawing API that can be
+//! implemented for various platforms.
+//!
+//! Druid is a data-driven, declarative framework. You describe your application
+//! model in terms of the [`Data`] trait, and then you build up a tree of
+//! [`widget`] s that can display and modify your data.
+//!
+//! Your widgets handle [`Event`]s, such as mouse movement, and can modify the data;
+//! these changes are then delivered to relevant widgets, which can update
+//! their state and redraw.
+//!
+//! As your application grows, you can use [`Lens`]es to expose only certain
+//! subsets of your data model to certains subsets of your widget tree.
+//!
+//! # Examples
+//!
+//! For many more examples, see [`druid/examples`].
+//!
+//! ```no_run
+//! use druid::widget::{Align, CrossAxisAlignment, Flex, Label, TextBox, WidgetExt};
+//! use druid::{AppLauncher, Data, Env, Lens, LocalizedString, Widget, WindowDesc};
+//!
+//! const VERTICAL_WIDGET_SPACING: f64 = 20.0;
+//! const TEXT_BOX_WIDTH: f64 = 200.0;
+//! const WINDOW_TITLE: LocalizedString<HelloState> = LocalizedString::new("Hello World!");
+//!
+//! #[derive(Clone, Data, Lens)]
+//! struct HelloState {
+//!     name: String,
+//! }
+//!
+//! fn main() {
+//!     // describe the main window
+//!     let main_window = WindowDesc::new(build_root_widget)
+//!         .title(WINDOW_TITLE)
+//!         .window_size((400.0, 400.0));
+//!
+//!     // create the initial app state
+//!     let initial_state = HelloState {
+//!         name: "World".into(),
+//!     };
+//!
+//!     // start the application
+//!     AppLauncher::with_window(main_window)
+//!         .launch(initial_state)
+//!         .expect("Failed to launch application");
+//! }
+//!
+//! fn build_root_widget() -> impl Widget<HelloState> {
+//!     // a label that will determine its text based on the current app data.
+//!     let label = Label::new(|data: &HelloState, _env: &Env| format!("Hello {}!", data.name));
+//!     // a textbox that modifies `name`.
+//!     let textbox = TextBox::new()
+//!         .with_placeholder("Who are we greeting?")
+//!         .fix_width(TEXT_BOX_WIDTH)
+//!         .lens(HelloState::name);
+//!
+//!     // arrange the two widgets vertically, with some padding
+//!     let layout = Flex::column()
+//!         // center the items horizontally in the container
+//!         .cross_axis_alignment(CrossAxisAlignment::Center)
+//!         .with_child(label, 0.0)
+//!         .with_spacer(VERTICAL_WIDGET_SPACING)
+//!         .with_child(textbox, 0.0);
+//!
+//!     // center the two widgets in the available space
+//!     Align::centered(layout)
+//! }
+//! ```
+//!
+//! [`Widget`]: widget/trait.Widget.html
+//! [`Data`]: trait.Data.html
+//! [`Lens`]: lens/trait.Lens.html
+//! [`widget`]: ./widget/index.html
+//! [`Event`]: enum.Event.html
+//! [`druid-shell`]: https://docs.rs/druid-shell
+//! [`piet`]: https://docs.rs/piet
+//! [`druid/examples`]: https://github.com/xi-editor/druid/tree/v0.4.0/druid/examples
 
 #![deny(intra_doc_link_resolution_failure, unsafe_code)]
-#![allow(clippy::new_ret_no_self)]
+#![allow(clippy::new_ret_no_self, clippy::needless_doctest_main)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
 
 use druid_shell as shell;
