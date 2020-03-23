3f271e1c76a64de9505078dc32945fc3b3cd6ba6
Paul Miller <paul@pauljmiller.com>
2020-03-23 16:43:00 -0400

    reimplement button using on_click

--- druid/src/widget/button.rs
+++ druid/src/widget/button.rs
@@ -13,25 +13,22 @@
 // limitations under the License.
 
 //! A button widget.
-
 use crate::theme;
-use crate::widget::{Label, LabelText};
+use crate::widget::{Label, LabelText, Painter};
 use crate::{
     Affine, BoxConstraints, Data, Env, Event, EventCtx, Insets, LayoutCtx, LifeCycle, LifeCycleCtx,
     LinearGradient, PaintCtx, Point, Rect, RenderContext, Size, UnitPoint, UpdateCtx, Widget,
+    WidgetExt,
 };
 
 // the minimum padding added to a button.
 // NOTE: these values are chosen to match the existing look of TextBox; these
 // should be reevaluated at some point.
-const LABEL_INSETS: Insets = Insets::uniform_xy(8., 2.);
+const LABEL_INSETS: Insets = Insets::uniform_xy(8., 4.);
 
 /// A button with a text label.
 pub struct Button<T> {
-    label: Label<T>,
-    label_size: Size,
-    /// A closure that will be invoked when the button is clicked.
-    action: Box<dyn Fn(&mut EventCtx, &mut T, &Env)>,
+    phantom: std::marker::PhantomData<T>,
 }
 
 impl<T: Data> Button<T> {
@@ -40,12 +37,52 @@ impl<T: Data> Button<T> {
     pub fn new(
         text: impl Into<LabelText<T>>,
         action: impl Fn(&mut EventCtx, &mut T, &Env) + 'static,
-    ) -> Button<T> {
-        Button {
-            label: Label::new(text),
-            label_size: Size::ZERO,
-            action: Box::new(action),
-        }
+    ) -> impl Widget<T> {
+        let painter = Self::painter();
+        CenteredLabel::new(text)
+            .padding(LABEL_INSETS)
+            .background(painter)
+            .on_click(action)
+    }
+
+    fn painter() -> Painter<T> {
+        Painter::new(|ctx, _, env| {
+            let is_active = ctx.is_active();
+            let is_hot = ctx.is_hot();
+            let size = ctx.size();
+
+            let rounded_rect = Rect::from_origin_size(Point::ORIGIN, size)
+                .inset(-1.0)
+                .to_rounded_rect(env.get(theme::BUTTON_BORDER_RADIUS));
+
+            let bg_gradient = if is_active {
+                LinearGradient::new(
+                    UnitPoint::TOP,
+                    UnitPoint::BOTTOM,
+                    (env.get(theme::BUTTON_DARK), env.get(theme::BUTTON_LIGHT)),
+                )
+            } else {
+                LinearGradient::new(
+                    UnitPoint::TOP,
+                    UnitPoint::BOTTOM,
+                    (env.get(theme::BUTTON_LIGHT), env.get(theme::BUTTON_DARK)),
+                )
+            };
+
+            let border_color = if is_hot {
+                env.get(theme::BORDER_LIGHT)
+            } else {
+                env.get(theme::BORDER_DARK)
+            };
+
+            ctx.stroke(
+                rounded_rect,
+                &border_color,
+                env.get(theme::BUTTON_BORDER_WIDTH),
+            );
+
+            ctx.fill(rounded_rect, &bg_gradient);
+        })
     }
 
     /// A function that can be passed to `Button::new`, for buttons with no action.
@@ -60,25 +97,22 @@ impl<T: Data> Button<T> {
     pub fn noop(_: &mut EventCtx, _: &mut T, _: &Env) {}
 }
 
-impl<T: Data> Widget<T> for Button<T> {
-    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut T, env: &Env) {
-        match event {
-            Event::MouseDown(_) => {
-                ctx.set_active(true);
-                ctx.request_paint();
-            }
-            Event::MouseUp(_) => {
-                if ctx.is_active() {
-                    ctx.set_active(false);
-                    ctx.request_paint();
-                    if ctx.is_hot() {
-                        (self.action)(ctx, data, env);
-                    }
-                }
-            }
-            _ => (),
+struct CenteredLabel<T> {
+    label: Label<T>,
+    label_size: Size,
+}
+
+impl<T: Data> CenteredLabel<T> {
+    pub fn new(text: impl Into<LabelText<T>>) -> CenteredLabel<T> {
+        CenteredLabel {
+            label: Label::new(text),
+            label_size: Size::ZERO,
         }
     }
+}
+
+impl<T: Data> Widget<T> for CenteredLabel<T> {
+    fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut T, _env: &Env) {}
 
     fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &T, env: &Env) {
         if let LifeCycle::HotChanged(_) = event {
@@ -98,57 +132,14 @@ impl<T: Data> Widget<T> for Button<T> {
         data: &T,
         env: &Env,
     ) -> Size {
-        bc.debug_check("Button");
-        let padding = Size::new(LABEL_INSETS.x_value(), LABEL_INSETS.y_value());
-        let label_bc = bc.shrink(padding).loosen();
+        bc.debug_check("CenteredLabel");
+        let label_bc = bc.loosen();
         self.label_size = self.label.layout(layout_ctx, &label_bc, data, env);
-        // HACK: to make sure we look okay at default sizes when beside a textbox,
-        // we make sure we will have at least the same height as the default textbox.
-        let min_height = env.get(theme::BORDERED_WIDGET_HEIGHT);
-
-        bc.constrain(Size::new(
-            self.label_size.width + padding.width,
-            (self.label_size.height + padding.height).max(min_height),
-        ))
+        bc.constrain(self.label_size)
     }
 
     fn paint(&mut self, ctx: &mut PaintCtx, data: &T, env: &Env) {
-        let is_active = ctx.is_active();
-        let is_hot = ctx.is_hot();
-        let size = ctx.size();
-
-        let rounded_rect = Rect::from_origin_size(Point::ORIGIN, size)
-            .to_rounded_rect(env.get(theme::BUTTON_BORDER_RADIUS));
-
-        let bg_gradient = if is_active {
-            LinearGradient::new(
-                UnitPoint::TOP,
-                UnitPoint::BOTTOM,
-                (env.get(theme::BUTTON_LIGHT), env.get(theme::BUTTON_DARK)),
-            )
-        } else {
-            LinearGradient::new(
-                UnitPoint::TOP,
-                UnitPoint::BOTTOM,
-                (env.get(theme::BUTTON_DARK), env.get(theme::BUTTON_LIGHT)),
-            )
-        };
-
-        let border_color = if is_hot {
-            env.get(theme::BORDER_LIGHT)
-        } else {
-            env.get(theme::BORDER_DARK)
-        };
-
-        ctx.stroke(
-            rounded_rect,
-            &border_color,
-            env.get(theme::BUTTON_BORDER_WIDTH),
-        );
-
-        ctx.fill(rounded_rect, &bg_gradient);
-
-        let label_offset = (size.to_vec2() - self.label_size.to_vec2()) / 2.0;
+        let label_offset = (ctx.size().to_vec2() - self.label_size.to_vec2()) / 2.0;
 
         ctx.with_save(|ctx| {
             ctx.transform(Affine::translate(label_offset));
