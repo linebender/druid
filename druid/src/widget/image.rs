06959ea20e487fbe63842a254b9fe5e1e4df5332
keenz <kindlychung@gmail.com>
2020-03-23 16:38:56 +0100

    remove unwanted phantom data field from image (#667)
    * remove unwanted phantom data field from image
    
    * remove unused
    
    * fmt
    
    * accomodate the image example
    
    * accomodate the image example
    
    * add T: Data back
    
    * fmt
    
    * remove phantom data in svg
    
    * fix data type
    
    Co-authored-by: Kaiyin Zhong <kindlychung@gmai.com>

--- druid/src/widget/image.rs
+++ druid/src/widget/image.rs
@@ -17,7 +17,6 @@
 
 use std::convert::AsRef;
 use std::error::Error;
-use std::marker::PhantomData;
 use std::path::Path;
 
 use image;
@@ -30,21 +29,19 @@ use crate::{
 };
 
 /// A widget that renders an Image
-pub struct Image<T> {
+pub struct Image {
     image_data: ImageData,
-    phantom: PhantomData<T>,
     fill: FillStrat,
     interpolation: InterpolationMode,
 }
 
-impl<T: Data> Image<T> {
+impl Image {
     /// Create an image drawing widget from `ImageData`.
     ///
     /// The Image will scale to fit its box constraints.
     pub fn new(image_data: ImageData) -> Self {
         Image {
             image_data,
-            phantom: Default::default(),
             fill: FillStrat::default(),
             interpolation: InterpolationMode::Bilinear,
         }
@@ -73,7 +70,7 @@ impl<T: Data> Image<T> {
     }
 }
 
-impl<T: Data> Widget<T> for Image<T> {
+impl<T: Data> Widget<T> for Image {
     fn event(&mut self, _ctx: &mut EventCtx, _event: &Event, _data: &mut T, _env: &Env) {}
 
     fn lifecycle(&mut self, _ctx: &mut LifeCycleCtx, _event: &LifeCycle, _data: &T, _env: &Env) {}
