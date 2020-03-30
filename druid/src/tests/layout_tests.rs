15898a23d6a3b51ddf6079532b7fc19d384a0095
Points Waves <pointswaves@gmail.com>
2020-03-30 18:26:00 +0100

    Add second call back so test can inspect what was painted

--- druid/src/tests/layout_tests.rs
+++ druid/src/tests/layout_tests.rs
@@ -31,7 +31,7 @@ fn simple_layout() {
         .with_id(id_1)
         .center();
 
-    Harness::create(true, widget, |harness| {
+    Harness::create_simple(true, widget, |harness| {
         harness.send_initial_events();
         harness.just_layout();
         let state = harness.get_state(id_1);
@@ -64,7 +64,7 @@ fn row_column() {
             1.0,
         );
 
-    Harness::create((), widget, |harness| {
+    Harness::create_simple((), widget, |harness| {
         harness.send_initial_events();
         harness.just_layout();
         let state1 = harness.get_state(id1);
@@ -95,7 +95,7 @@ fn simple_paint_rect() {
         .background(Color::BLACK)
         .center();
 
-    Harness::create((), widget, |harness| {
+    Harness::create_simple((), widget, |harness| {
         harness.send_initial_events();
         harness.just_layout();
 
@@ -170,7 +170,7 @@ fn flex_paint_rect_overflow() {
         .padding(10.)
         .center();
 
-    Harness::create((), widget, |harness| {
+    Harness::create_simple((), widget, |harness| {
         harness.set_initial_size(Size::new(300., 300.));
         harness.send_initial_events();
         harness.just_layout();
--- druid/src/tests/mod.rs
+++ druid/src/tests/mod.rs
@@ -83,7 +83,7 @@ fn propogate_hot() {
         }
     }
     #[allow(clippy::cognitive_complexity)]
-    Harness::create((), widget, |harness| {
+    Harness::create_simple((), widget, |harness| {
         harness.send_initial_events();
         harness.just_layout();
 
@@ -179,7 +179,7 @@ fn take_focus() {
     let app = Split::columns(left, right).padding(5.0);
     let data = true;
 
-    Harness::create(data, app, |harness| {
+    Harness::create_simple(data, app, |harness| {
         harness.send_initial_events();
         // nobody should have focus
         assert!(left_focus.get().is_none());
@@ -203,7 +203,7 @@ fn take_focus() {
 fn simple_lifecyle() {
     let record = Recording::default();
     let widget = SizedBox::empty().record(&record);
-    Harness::create(true, widget, |harness| {
+    Harness::create_simple(true, widget, |harness| {
         harness.send_initial_events();
         assert_matches!(record.next(), Record::L(LifeCycle::WidgetAdded));
         assert_matches!(record.next(), Record::E(Event::WindowConnected));
@@ -226,7 +226,7 @@ fn adding_child_lifecycle() {
 
     let widget = Split::columns(Label::new("hi").record(&record), replacer);
 
-    Harness::create(String::new(), widget, |harness| {
+    Harness::create_simple(String::new(), widget, |harness| {
         harness.send_initial_events();
 
         assert_matches!(record.next(), Record::L(LifeCycle::WidgetAdded));
@@ -262,7 +262,7 @@ fn participate_in_autofocus() {
         replacer,
     );
 
-    Harness::create("my test text".to_string(), widget, |harness| {
+    Harness::create_simple("my test text".to_string(), widget, |harness| {
         // verify that all widgets are marked as having children_changed
         // (this should always be true for a new widget)
         harness.inspect_state(|state| assert!(state.children_changed));
@@ -297,7 +297,7 @@ fn child_tracking() {
     .padding(5.0)
     .with_id(id_4);
 
-    Harness::create(true, widget, |harness| {
+    Harness::create_simple(true, widget, |harness| {
         harness.send_initial_events();
         let root = harness.get_state(id_4);
         assert_eq!(root.children.entry_count(), 3);
@@ -325,7 +325,7 @@ fn register_after_adding_child() {
 
     let widget = Split::columns(Label::new("hi").with_id(id_4), replacer).with_id(id_5);
 
-    Harness::create(String::new(), widget, |harness| {
+    Harness::create_simple(String::new(), widget, |harness| {
         harness.send_initial_events();
 
         assert!(harness.get_state(id_5).children.may_contain(&id_6));
