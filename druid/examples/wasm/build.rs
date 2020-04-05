5609e38252fb3226e5ed22eb8fdbc3f72df73b6c
Egor Larionov <egor.larionov@gmail.com>
2020-04-05 13:14:06 -0700

    Fix the build script for wasm examples
    This addresses the CI failures caused by the weird build of the wasm
    examples.
    
    To elaborate:
    In order to include each of the examples in the wasm example, a
    symlink to the examples directory is created in build.rs along with the
    corresponding examples.rs module which declares examples known to work
    with wasm. To satisfy CI, the examples.rs committed to the repo must be
    empty since rustfmt does not call "build.rs".

--- druid/examples/wasm/build.rs
+++ druid/examples/wasm/build.rs
@@ -2,9 +2,16 @@ use std::io::Result;
 use std::path::PathBuf;
 use std::{env, fs};
 
+/// Examples known to not work with WASM are skipped. Ideally this list will eventually be empty.
+const EXCEPTIONS: &[&'static str] = &[
+    "svg",       // usvg doesn't currently build with WASM.
+    "ext_event", // WASM doesn't currently support spawning threads.
+];
+
 fn main() -> Result<()> {
     let crate_dir = PathBuf::from(&env::var("CARGO_MANIFEST_DIR").unwrap());
-    let examples_dir = crate_dir.join("src").join("examples");
+    let src_dir = crate_dir.join("src");
+    let examples_dir = src_dir.join("examples");
 
     let parent_dir = crate_dir.parent().unwrap();
 
@@ -14,7 +21,20 @@ fn main() -> Result<()> {
     #[cfg(windows)]
     std::os::windows::fs::symlink_dir(parent_dir, &examples_dir).ok();
 
-    // Get a list of examples
+    // Generate example module and the necessary html documents.
+
+    // Declare the newly found example modules in examples.rs
+    let mut example_rs = r#"
+//! This is an automatically generated module collecting all valid examples
+//! in the parent examples directory.
+
+// This file must not be committed unless it contains no modules.
+// Rustfmt will fail if this file is not empty in the repository --- it does not run the
+// "build.rs" script, and the examples module directory symlink is created at build time in
+// "build.rs".
+
+"#
+    .to_string();
 
     for entry in examples_dir.read_dir()? {
         let path = entry?.path();
@@ -28,43 +48,52 @@ fn main() -> Result<()> {
 
         if let Some(example) = path.file_stem() {
             let example_str = example.to_string_lossy();
+
+            // Skip examples that are known to not work.
+            if EXCEPTIONS.contains(&example_str.as_ref()) {
+                continue;
+            }
+
+            // Record the valid example module we found to add to the generated examples.rs
+            example_rs.push_str(&format!("pub mod {};\n", example_str));
+
             // Create an html document for each example.
             let html = format!(
                 r#"
-        <!DOCTYPE html>
-        <html lang="en">
-            <head>
-                <meta charset="utf-8">
-                <title>Druid WASM example - {name}</title>
-                <style>
-                    html, body, canvas {{
-                        margin: 0px;
-                        padding: 0px;
-                        width: 100%;
-                        height: 100%;
-                        overflow: hidden;
-                    }}
-                </style>
-            </head>
-            <body>
-                <noscript>This page contains webassembly and javascript content, please enable javascript in your browser.</noscript>
-                <canvas id="canvas"></canvas>
-                <script type="module">
-                    import init, {{ {name} }} from '../pkg/druid_wasm_examples.js';
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Druid WASM example - {name}</title>
+        <style>
+            html, body, canvas {{
+                margin: 0px;
+                padding: 0px;
+                width: 100%;
+                height: 100%;
+                overflow: hidden;
+            }}
+        </style>
+    </head>
+    <body>
+        <noscript>This page contains webassembly and javascript content, please enable javascript in your browser.</noscript>
+        <canvas id="canvas"></canvas>
+        <script type="module">
+            import init, {{ {name} }} from '../pkg/druid_wasm_examples.js';
 
-                    async function run() {{
-                        await init();
-                        {name}();
-                    }}
+            async function run() {{
+                await init();
+                {name}();
+            }}
 
-                    run();
-                </script>
-            </body>
-        </html>
-        "#,
+            run();
+        </script>
+    </body>
+</html>"#,
                 name = example_str
             );
 
+            // Write out the html file into a designated html directory located in crate root.
             let html_dir = crate_dir.join("html");
             if !html_dir.exists() {
                 fs::create_dir(&html_dir).unwrap_or_else(|_| {
@@ -76,5 +105,9 @@ fn main() -> Result<()> {
                 .unwrap_or_else(|_| panic!("Failed to create {}.html", example_str));
         }
     }
+
+    // Write out the contents of the examples.rs module.
+    //fs::write(src_dir.join("examples.rs"), example_rs)?;
+
     Ok(())
 }
--- druid/examples/wasm/src/examples.rs
+++ druid/examples/wasm/src/examples.rs
@@ -1,23 +1,2 @@
-pub mod anim;
-pub mod calc;
-pub mod custom_widget;
-pub mod either;
-pub mod flex;
-pub mod game_of_life;
-pub mod hello;
-pub mod identity;
-pub mod image; // Can't load an image from a local file
-pub mod layout;
-pub mod lens;
-pub mod list;
-pub mod multiwin;
-pub mod panels;
-pub mod parse;
-pub mod scroll;
-pub mod scroll_colors;
-pub mod split_demo;
-pub mod styled_text;
-//pub mod svg; // usvg doesn't compile on usvg at the time of this writing
-pub mod switch;
-pub mod timer;
-pub mod view_switcher;
+//! This module is intentionally left blank.
+//! The contents are automatically generated in the "build.rs" script.
