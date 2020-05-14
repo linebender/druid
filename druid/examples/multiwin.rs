b8cbd5ba9aa9539293542f426056ce0c404af0e5
Kaiyin Zhong <kindlychung@gmai.com>
2020-05-14 13:39:31 +0200

    fmt

--- druid/examples/multiwin.rs
+++ druid/examples/multiwin.rs
@@ -57,16 +57,14 @@ fn ui_builder() -> impl Widget<State> {
     let text = LocalizedString::new("hello-counter")
         .with_arg("count", |data: &State, _env| data.menu_count.into());
     let label = Label::new(text);
-    let inc_button =
-        Button::<State>::new("Add menu item").on_click(|ctx, data, _env| {
-            data.menu_count += 1;
-            ctx.submit_command(MENU_INCREMENT_ACTION, ctx.window_id())
-        });
-    let dec_button =
-        Button::<State>::new("Remove menu item").on_click(|ctx, data, _env| {
-            data.menu_count = data.menu_count.saturating_sub(1);
-            ctx.submit_command(MENU_DECREMENT_ACTION, ctx.window_id())
-        });
+    let inc_button = Button::<State>::new("Add menu item").on_click(|ctx, data, _env| {
+        data.menu_count += 1;
+        ctx.submit_command(MENU_INCREMENT_ACTION, ctx.window_id())
+    });
+    let dec_button = Button::<State>::new("Remove menu item").on_click(|ctx, data, _env| {
+        data.menu_count = data.menu_count.saturating_sub(1);
+        ctx.submit_command(MENU_DECREMENT_ACTION, ctx.window_id())
+    });
     let new_button = Button::<State>::new("New window").on_click(|ctx, _data, _env| {
         ctx.submit_command(sys_cmds::NEW_FILE, Target::Global);
     });
