8b0e298af78c905d4efef82d188981f38871f67b
covercash2 <covercash2@gmail.com>
2020-06-01 19:00:33 -0700

    export `Image` and `ImageData`
    By reducing the scope of the "image" feature, we can
    expose the bare-bones image widget API and better
    document the "image" feature.

--- druid/src/widget/image.rs
+++ druid/src/widget/image.rs
@@ -15,10 +15,6 @@
 //! An Image widget.
 //! Please consider using SVG and the SVG wideget as it scales much better.
 
-use std::convert::AsRef;
-use std::error::Error;
-use std::path::Path;
-
 use crate::{
     piet::{ImageFormat, InterpolationMode},
     widget::common::FillStrat,
@@ -161,6 +157,34 @@ impl ImageData {
         }
     }
 
+    /// Get the size in pixels of the contained image.
+    fn get_size(&self) -> Size {
+        Size::new(self.x_pixels as f64, self.y_pixels as f64)
+    }
+
+    /// Convert ImageData into Piet draw instructions.
+    fn to_piet(&self, offset_matrix: Affine, ctx: &mut PaintCtx, interpolation: InterpolationMode) {
+        ctx.with_save(|ctx| {
+            ctx.transform(offset_matrix);
+            let size = self.get_size();
+            let im = ctx
+                .make_image(
+                    size.width as usize,
+                    size.height as usize,
+                    &self.pixels,
+                    self.format,
+                )
+                .unwrap();
+            ctx.draw_image(&im, size.to_rect(), interpolation);
+        })
+    }
+}
+
+#[cfg(feature = "image")]
+use std::{convert::AsRef, error::Error, path::Path};
+#[cfg(feature = "image")]
+#[cfg_attr(docsrs, doc(cfg(feature = "image")))]
+impl ImageData {
     /// Load an image from a DynamicImage from the image crate
     pub fn from_dynamic_image(image_data: image::DynamicImage) -> ImageData {
         if has_alpha_channel(&image_data) {
@@ -207,30 +231,10 @@ impl ImageData {
         let image_data = image::open(path).map_err(|e| e)?;
         Ok(ImageData::from_dynamic_image(image_data))
     }
-
-    /// Get the size in pixels of the contained image.
-    fn get_size(&self) -> Size {
-        Size::new(self.x_pixels as f64, self.y_pixels as f64)
-    }
-
-    /// Convert ImageData into Piet draw instructions.
-    fn to_piet(&self, offset_matrix: Affine, ctx: &mut PaintCtx, interpolation: InterpolationMode) {
-        ctx.with_save(|ctx| {
-            ctx.transform(offset_matrix);
-            let size = self.get_size();
-            let im = ctx
-                .make_image(
-                    size.width as usize,
-                    size.height as usize,
-                    &self.pixels,
-                    self.format,
-                )
-                .unwrap();
-            ctx.draw_image(&im, size.to_rect(), interpolation);
-        })
-    }
 }
 
+#[cfg(feature = "image")]
+#[cfg_attr(docsrs, doc(cfg(feature = "image")))]
 fn has_alpha_channel(image: &image::DynamicImage) -> bool {
     use image::ColorType::*;
     match image.color() {
