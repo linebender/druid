linebender/druid

commit 861bfbff5023a2ade607cf431621519e436f0ad8

Basic integration with new Piet text API
This introduces a new type, TextLayout, that is intended to be
a component used by widgets that wish to display text, and which
exposes the functionality of the Piet text API.

This commit is a checkpoint; it adds only a small amount of new
functionality, and largely tries to keep things working as they
do currently.

druid/examples/styled_text.rs
@@ -16,14 +16,13 @@
 
 use druid::widget::{Checkbox, Flex, Label, MainAxisAlignment, Painter, Parse, Stepper, TextBox};
 use druid::{
-    theme, AppLauncher, ArcStr, Color, Data, Key, Lens, LensExt, LensWrap, LocalizedString,
-    PlatformError, RenderContext, Widget, WidgetExt, WindowDesc,
+    theme, AppLauncher, Color, Data, FontDescriptor, FontFamily, Key, Lens, LensExt, LensWrap,
+    LocalizedString, PlatformError, RenderContext, Widget, WidgetExt, WindowDesc,
 };
 use std::fmt::Display;
 
-// This is a custom key we'll use with Env to set and get our text size.
-const MY_CUSTOM_TEXT_SIZE: Key<f64> = Key::new("styled_text.custom_text_size");
-const MY_CUSTOM_FONT: Key<ArcStr> = Key::new("styled_text.custom_font");
+// This is a custom key we'll use with Env to set and get our font.
+const MY_CUSTOM_FONT: Key<FontDescriptor> = Key::new("styled_text.custom_font");
 
 #[derive(Clone, Lens, Data)]
 struct AppData {
@@ -73,7 +72,7 @@ fn ui_builder() -> impl Widget<AppData> {
     });
 
     // This is druid's default text style.
-    // It's set by theme::LABEL_COLOR, theme::TEXT_SIZE_NORMAL, and theme::FONT_NAME
+    // It's set by theme::LABEL_COLOR and theme::UI_FONT
     let label =
         Label::new(|data: &String, _env: &_| format!("Default: {}", data)).lens(AppData::text);
 
@@ -88,19 +87,19 @@ fn ui_builder() -> impl Widget<AppData> {
     // to the default font)
     let styled_label = Label::new(|data: &AppData, _env: &_| format!("{}", data))
         .with_text_color(theme::PRIMARY_LIGHT)
-        .with_text_size(MY_CUSTOM_TEXT_SIZE)
         .with_font(MY_CUSTOM_FONT)
         .background(my_painter)
         .on_click(|_, data, _| {
             data.size *= 1.1;
         })
         .env_scope(|env: &mut druid::Env, data: &AppData| {
-            env.set(MY_CUSTOM_TEXT_SIZE, data.size);
-            if data.mono {
-                env.set(MY_CUSTOM_FONT, "monospace");
+            let new_font = if data.mono {
+                FontDescriptor::new(FontFamily::MONOSPACE)
             } else {
-                env.set(MY_CUSTOM_FONT, env.get(theme::FONT_NAME));
+                FontDescriptor::new(FontFamily::SYSTEM_UI)
             }
+            .with_size(data.size);
+            env.set(MY_CUSTOM_FONT, new_font);
         });
 
     let stepper = Stepper::new()
@@ -118,7 +117,10 @@ fn ui_builder() -> impl Widget<AppData> {
 
     let mono_checkbox = Checkbox::new("Monospace").lens(AppData::mono);
 
-    let input = TextBox::new().fix_width(200.0).lens(AppData::text);
+    let input = TextBox::new()
+        .with_text_size(38.0)
+        .fix_width(200.0)
+        .lens(AppData::text);
 
     Flex::column()
         .main_axis_alignment(MainAxisAlignment::Center)
@@ -131,4 +133,5 @@ fn ui_builder() -> impl Widget<AppData> {
         .with_child(mono_checkbox)
         .with_spacer(8.0)
         .with_child(input.padding(5.0))
+        .debug_widget_id()
 }

druid/src/text/layout.rs
@@ -1,0 +1,267 @@
+// Copyright 2020 The xi-editor Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! A type for laying out, drawing, and interacting with text.
+
+use std::ops::Range;
+
+use crate::kurbo::{Line, Point, Rect, Size};
+use crate::piet::{
+    Color, PietText, PietTextLayout, Text as _, TextAttribute, TextLayout as _,
+    TextLayoutBuilder as _,
+};
+use crate::{ArcStr, Data, Env, FontDescriptor, KeyOrValue, PaintCtx, RenderContext};
+
+/// A component for displaying text on screen.
+///
+/// This is a type intended to be used by other widgets that display text.
+/// It allows for the text itself as well as font and other styling information
+/// to be set and modified. It wraps an inner layout object, and handles
+/// invalidating and rebuilding it as required.
+///
+/// This object is not valid until the [`rebuild_if_needed`] method has been
+/// called. Additionally, this method must be called anytime the text or
+/// other properties have changed, or if any  items in the [`Env`] that are
+/// referenced in this layout change. In general, you should just call this
+/// method as part of your widget's `update` method.
+///
+/// [`rebuild_if_needed`]: #method.rebuild_if_needed
+/// [`Env`]: struct.Env.html
+#[derive(Clone)]
+pub struct TextLayout {
+    text: ArcStr,
+    font: KeyOrValue<FontDescriptor>,
+    text_size_override: Option<KeyOrValue<f64>>,
+    text_color: KeyOrValue<Color>,
+    //FIXME: all this caching stuff can go away when we have a simple way of
+    // checking if something has changed in the env.
+    cached_text_color: Color,
+    cached_font: FontDescriptor,
+    // when set, this will be used to override the size in he font descriptor.
+    // This provides an easy way to change only the font size, while still
+    // using a `FontDescriptor` in the `Env`.
+    cached_text_size: Option<f64>,
+    // the underlying layout object. This is constructed lazily.
+    layout: Option<PietTextLayout>,
+}
+
+impl TextLayout {
+    /// Create a new `TextLayout` object.
+    ///
+    /// You do not provide the actual text at creation time; instead you pass
+    /// it in when calling [`rebuild_if_needed`].
+    ///
+    /// [`rebuild_if_needed`]: #method.rebuild_if_needed
+    pub fn new(text: impl Into<ArcStr>) -> Self {
+        TextLayout {
+            text: text.into(),
+            font: crate::theme::UI_FONT.into(),
+            cached_font: Default::default(),
+            text_color: crate::theme::LABEL_COLOR.into(),
+            cached_text_color: Color::BLACK,
+            text_size_override: None,
+            cached_text_size: None,
+            layout: None,
+        }
+    }
+
+    /// Returns `true` if this layout needs to be rebuilt.
+    ///
+    /// This happens (for instance) after style attributes are modified.
+    ///
+    /// This does not account for things like the text changing, handling that
+    /// is the responsibility of the user.
+    pub fn needs_rebuild(&self) -> bool {
+        self.layout.is_none()
+    }
+
+    /// Set the text to display.
+    pub fn set_text(&mut self, text: impl Into<ArcStr>) {
+        self.text = text.into();
+        self.layout = None;
+    }
+
+    /// Set the default text color for this layout.
+    pub fn set_text_color(&mut self, color: impl Into<KeyOrValue<Color>>) {
+        self.text_color = color.into();
+        self.layout = None;
+    }
+
+    /// Set the default font.
+    ///
+    /// The argument is a [`FontDescriptor`] or a [`Key<FontDescriptor>`] that
+    /// can be resolved from the [`Env`].
+    ///
+    /// [`FontDescriptor`]: struct.FontDescriptor.html
+    /// [`Env`]: struct.Env.html
+    /// [`Key<FontDescriptor>`]: struct.Key.html
+    pub fn set_font(&mut self, font: impl Into<KeyOrValue<FontDescriptor>>) {
+        self.font = font.into();
+        self.layout = None;
+        self.text_size_override = None;
+    }
+
+    /// Set the font size.
+    ///
+    /// This overrides the size in the [`FontDescriptor`] provided to [`set_font`].
+    ///
+    /// [`set_font`]: #method.set_font.html
+    /// [`FontDescriptor`]: struct.FontDescriptor.html
+    pub fn set_text_size(&mut self, size: impl Into<KeyOrValue<f64>>) {
+        self.text_size_override = Some(size.into());
+        self.layout = None;
+    }
+
+    /// The size of the laid-out text.
+    ///
+    /// This is not meaningful until [`rebuild_if_needed`] has been called.
+    ///
+    /// [`rebuild_if_needed`]: #method.rebuild_if_needed
+    pub fn size(&self) -> Size {
+        self.layout
+            .as_ref()
+            .map(|layout| layout.size())
+            .unwrap_or_default()
+    }
+
+    /// For a given `Point` (relative to this object's origin), returns index
+    /// into the underlying text of the nearest grapheme boundary.
+    pub fn text_position_for_point(&self, point: Point) -> usize {
+        self.layout
+            .as_ref()
+            .map(|layout| layout.hit_test_point(point).idx)
+            .unwrap_or_default()
+    }
+
+    /// Given the utf-8 position of a character boundary in the underlying text,
+    /// return the `Point` (relative to this object's origin) representing the
+    /// boundary of the containing grapheme.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `text_pos` is not a character boundary.
+    pub fn point_for_text_position(&self, text_pos: usize) -> Point {
+        self.layout
+            .as_ref()
+            .map(|layout| layout.hit_test_text_position(text_pos).point)
+            .unwrap_or_default()
+    }
+
+    /// Given a utf-8 range in the underlying text, return a `Vec` of `Rect`s
+    /// representing the nominal bounding boxes of the text in that range.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the range start or end is not a character boundary.
+    pub fn rects_for_range(&self, range: Range<usize>) -> Vec<Rect> {
+        self.layout
+            .as_ref()
+            .map(|layout| layout.rects_for_range(range))
+            .unwrap_or_default()
+    }
+
+    /// Given the utf-8 position of a character boundary in the underlying text,
+    /// return a `Line` suitable for drawing a vertical cursor at that boundary.
+    pub fn cursor_line_for_text_position(&self, text_pos: usize) -> Line {
+        self.layout
+            .as_ref()
+            .map(|layout| {
+                let pos = layout.hit_test_text_position(text_pos);
+                let line_metrics = layout.line_metric(pos.line).unwrap();
+                let p1 = (pos.point.x, line_metrics.y_offset);
+                let p2 = (pos.point.x, (line_metrics.y_offset + line_metrics.height));
+                dbg!(Line::new(p1, p2))
+            })
+            .unwrap_or_else(|| Line::new(Point::ZERO, Point::ZERO))
+    }
+
+    /// Called during the containing widgets `update` method; this text object
+    /// will check to see if any used environment items have changed,
+    /// and invalidate itself as needed.
+    ///
+    /// Returns `true` if an item has changed, indicating that the text object
+    /// needs layout.
+    ///
+    /// # Note
+    ///
+    /// After calling this method, the layout may be invalid until the next call
+    /// to [`rebuild_layout_if_needed`], [`layout`], or [`paint`].
+    ///
+    /// [`layout`]: #method.layout
+    /// [`paint`]: #method.paint
+    /// [`rebuild_layout_if_needed`]: #method.rebuild_layout_if_needed
+    pub fn rebuild_if_needed(&mut self, factory: &mut PietText, env: &Env) {
+        let new_font = self.font.resolve(env);
+        let new_color = self.text_color.resolve(env);
+        let new_size = self.text_size_override.as_ref().map(|key| key.resolve(env));
+
+        let needs_rebuild = !new_font.same(&self.cached_font)
+            || !new_color.same(&self.cached_text_color)
+            || new_size != self.cached_text_size
+            || self.layout.is_none();
+
+        self.cached_font = new_font;
+        self.cached_text_color = new_color;
+        self.cached_text_size = new_size;
+
+        if needs_rebuild {
+            let descriptor = if let Some(size) = &self.cached_text_size {
+                self.cached_font.clone().with_size(*size)
+            } else {
+                self.cached_font.clone()
+            };
+            let text_color = self.cached_text_color.clone();
+            self.layout = Some(
+                factory
+                    .new_text_layout(self.text.clone())
+                    .font(descriptor.family.clone(), descriptor.size)
+                    .default_attribute(descriptor.weight)
+                    .default_attribute(descriptor.style)
+                    .default_attribute(TextAttribute::ForegroundColor(text_color))
+                    .build()
+                    .unwrap(),
+            )
+        }
+    }
+
+    ///  Draw the layout at the provided `Point`.
+    ///
+    ///  The origin of the layout is the top-left corner.
+    ///
+    ///  You must call [`rebuild_if_needed`] at some point before you first
+    ///  call this method.
+    ///
+    ///  [`rebuild_if_needed`]: #method.rebuild_if_needed
+    pub fn draw(&self, ctx: &mut PaintCtx, point: impl Into<Point>) {
+        ctx.draw_text(self.layout.as_ref().unwrap(), point)
+    }
+}
+
+impl std::fmt::Debug for TextLayout {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("TextLayout")
+            .field("font", &self.font)
+            .field("text_size_override", &self.text_size_override)
+            .field("text_color", &self.text_color)
+            .field(
+                "layout",
+                if self.layout.is_some() {
+                    &"Some"
+                } else {
+                    &"None"
+                },
+            )
+            .finish()
+    }
+}

druid/src/widget/textbox.rs
@@ -16,26 +16,22 @@
 
 use std::time::Duration;
 
+use crate::widget::prelude::*;
 use crate::{
-    Application, BoxConstraints, Cursor, Env, Event, EventCtx, HotKey, KbKey, LayoutCtx, LifeCycle,
-    LifeCycleCtx, PaintCtx, Selector, SysMods, TimerToken, UpdateCtx, Widget,
+    Application, BoxConstraints, Cursor, Data, Env, FontDescriptor, HotKey, KbKey, KeyOrValue,
+    Selector, SysMods, TimerToken,
 };
 
-use crate::kurbo::{Affine, Line, Point, Size, Vec2};
-use crate::piet::{
-    FontFamily, PietText, PietTextLayout, RenderContext, Text, TextAttribute, TextLayout,
-    TextLayoutBuilder,
-};
+use crate::kurbo::{Affine, Insets, Point, Size};
 use crate::theme;
 
 use crate::text::{
     movement, offset_for_delete_backwards, BasicTextInput, EditAction, EditableText, MouseAction,
-    Movement, Selection, TextInput,
+    Movement, Selection, TextInput, TextLayout,
 };
 
 const BORDER_WIDTH: f64 = 1.;
-const PADDING_TOP: f64 = 5.;
-const PADDING_LEFT: f64 = 4.;
+const TEXT_INSETS: Insets = Insets::new(4.0, 2.0, 0.0, 2.0);
 
 // we send ourselves this when we want to reset blink, which must be done in event.
 const RESET_BLINK: Selector = Selector::new("druid-builtin.reset-textbox-blink");
@@ -45,6 +41,7 @@ const CURSOR_BLINK_DURATION: Duration = Duration::from_millis(500);
 #[derive(Debug, Clone)]
 pub struct TextBox {
     placeholder: String,
+    text: TextLayout,
     width: f64,
     hscroll_offset: f64,
     selection: Selection,
@@ -59,9 +56,11 @@ impl TextBox {
 
     /// Create a new TextBox widget
     pub fn new() -> TextBox {
+        let text = TextLayout::new("");
         Self {
             width: 0.0,
             hscroll_offset: 0.,
+            text,
             selection: Selection::caret(0),
             cursor_timer: TimerToken::INVALID,
             cursor_on: false,
@@ -75,44 +74,48 @@ impl TextBox {
         self
     }
 
-    #[deprecated(since = "0.5.0", note = "Use TextBox::new instead")]
-    #[doc(hidden)]
-    pub fn raw() -> TextBox {
-        Self::new()
+    /// Builder-style method for setting the text size.
+    ///
+    /// The argument can be either an `f64` or a [`Key<f64>`].
+    ///
+    /// [`Key<f64>`]: ../struct.Key.html
+    pub fn with_text_size(mut self, size: impl Into<KeyOrValue<f64>>) -> Self {
+        self.set_text_size(size);
+        self
     }
 
-    /// Calculate the PietTextLayout from the given text, font, and font size
-    fn get_layout(
-        &self,
-        piet_text: &mut PietText,
-        text: &str,
-        env: &Env,
-        use_placeholder_color: bool,
-    ) -> PietTextLayout {
-        let font_name = env.get(theme::FONT_NAME);
-        let font_size = env.get(theme::TEXT_SIZE_NORMAL);
-        let default_color = if use_placeholder_color {
-            env.get(theme::PLACEHOLDER_COLOR)
-        } else {
-            env.get(theme::LABEL_COLOR)
-        };
-        let selection_text_color = env.get(theme::SELECTION_TEXT_COLOR);
+    /// Builder-style method for setting the font.
+    ///
+    /// The argument can be a [`FontDescriptor`] or a [`Key<FontDescriptor>`]
+    /// that refers to a font defined in the [`Env`].
+    ///
+    /// [`Env`]: ../struct.Env.html
+    /// [`FontDescriptor`]: ../struct.FontDescriptor.html
+    /// [`Key<FontDescriptor>`]: ../struct.Key.html
+    pub fn with_font(mut self, font: impl Into<KeyOrValue<FontDescriptor>>) -> Self {
+        self.set_font(font);
+        self
+    }
 
-        // TODO: caching of both the format and the layout
-        let font = piet_text
-            .font_family(&font_name)
-            .unwrap_or(FontFamily::SYSTEM_UI);
+    /// Set the text size.
+    ///
+    /// The argument can be either an `f64` or a [`Key<f64>`].
+    ///
+    /// [`Key<f64>`]: ../struct.Key.html
+    pub fn set_text_size(&mut self, size: impl Into<KeyOrValue<f64>>) {
+        self.text.set_text_size(size);
+    }
 
-        piet_text
-            .new_text_layout(text.to_string())
-            .font(font, font_size)
-            .default_attribute(TextAttribute::ForegroundColor(default_color))
-            .range_attribute(
-                self.selection.range(),
-                TextAttribute::ForegroundColor(selection_text_color),
-            )
-            .build()
-            .unwrap()
+    /// Set the font.
+    ///
+    /// The argument can be a [`FontDescriptor`] or a [`Key<FontDescriptor>`]
+    /// that refers to a font defined in the [`Env`].
+    ///
+    /// [`Env`]: ../struct.Env.html
+    /// [`FontDescriptor`]: ../struct.FontDescriptor.html
+    /// [`Key<FontDescriptor>`]: ../struct.Key.html
+    pub fn set_font(&mut self, font: impl Into<KeyOrValue<FontDescriptor>>) {
+        self.text.set_font(font);
     }
 
     /// Insert text at the cursor position.
@@ -206,26 +209,26 @@ impl TextBox {
 
     /// For a given point, returns the corresponding offset (in bytes) of
     /// the grapheme cluster closest to that point.
-    fn offset_for_point(&self, point: Point, layout: &PietTextLayout) -> usize {
+    fn offset_for_point(&self, point: Point) -> usize {
         // Translating from screenspace to Piet's text layout representation.
         // We need to account for hscroll_offset state and TextBox's padding.
-        let translated_point = Point::new(point.x + self.hscroll_offset - PADDING_LEFT, point.y);
-        let hit_test = layout.hit_test_point(translated_point);
-        hit_test.idx
+        let translated_point = Point::new(point.x + self.hscroll_offset - TEXT_INSETS.x0, point.y);
+        self.text.text_position_for_point(translated_point)
     }
 
     /// Given an offset (in bytes) of a valid grapheme cluster, return
     /// the corresponding x coordinate of that grapheme on the screen.
-    fn x_for_offset(&self, layout: &PietTextLayout, offset: usize) -> f64 {
-        layout.hit_test_text_position(offset).point.x
+    fn x_pos_for_offset(&self, offset: usize) -> f64 {
+        self.text.point_for_text_position(offset).x
     }
 
     /// Calculate a stateful scroll offset
-    fn update_hscroll(&mut self, layout: &PietTextLayout) {
-        let cursor_x = self.x_for_offset(layout, self.cursor());
-        let overall_text_width = layout.size().width;
+    fn update_hscroll(&mut self) {
+        let cursor_x = self.x_pos_for_offset(self.cursor());
+        let overall_text_width = self.text.size().width;
 
-        let padding = PADDING_LEFT * 2.;
+        // when advancing the cursor, we want some additional padding
+        let padding = TEXT_INSETS.x0 * 2.;
         if overall_text_width < self.width {
             // There's no offset if text is smaller than text box
             //
@@ -257,8 +260,6 @@ impl Widget<String> for TextBox {
     fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut String, env: &Env) {
         // Guard against external changes in data?
         self.selection = self.selection.constrain_to(data);
-
-        let mut text_layout = self.get_layout(&mut ctx.text(), &data, env, data.is_empty());
         let mut edit_action = None;
 
         match event {
@@ -267,7 +268,7 @@ impl Widget<String> for TextBox {
                 ctx.set_active(true);
 
                 if !mouse.focus {
-                    let cursor_offset = self.offset_for_point(mouse.pos, &text_layout);
+                    let cursor_offset = self.offset_for_point(mouse.pos);
                     edit_action = Some(EditAction::Click(MouseAction {
                         row: 0,
                         column: cursor_offset,
@@ -280,7 +281,7 @@ impl Widget<String> for TextBox {
             Event::MouseMove(mouse) => {
                 ctx.set_cursor(&Cursor::IBeam);
                 if ctx.is_active() {
-                    let cursor_offset = self.offset_for_point(mouse.pos, &text_layout);
+                    let cursor_offset = self.offset_for_point(mouse.pos);
                     edit_action = Some(EditAction::Drag(MouseAction {
                         row: 0,
                         column: cursor_offset,
@@ -359,36 +360,63 @@ impl Widget<String> for TextBox {
 
             self.do_edit_action(edit_action, data);
             self.reset_cursor_blink(ctx);
+            self.text.set_text(data.as_str());
+            self.text.rebuild_if_needed(&mut ctx.text(), env);
 
             if !is_select_all {
-                text_layout = self.get_layout(&mut ctx.text(), &data, env, data.is_empty());
-                self.update_hscroll(&text_layout);
+                self.update_hscroll();
             }
         }
     }
 
-    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, _data: &String, _env: &Env) {
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &String, env: &Env) {
         match event {
-            LifeCycle::WidgetAdded => ctx.register_for_focus(),
+            LifeCycle::WidgetAdded => {
+                ctx.register_for_focus();
+                self.text.set_text(data.clone());
+                self.text.rebuild_if_needed(&mut ctx.text(), env);
+            }
             // an open question: should we be able to schedule timers here?
             LifeCycle::FocusChanged(true) => ctx.submit_command(RESET_BLINK.to(ctx.widget_id())),
             _ => (),
         }
     }
 
-    fn update(&mut self, ctx: &mut UpdateCtx, _old_data: &String, _data: &String, _env: &Env) {
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &String, data: &String, env: &Env) {
+        let content = if data.is_empty() {
+            &self.placeholder
+        } else {
+            data
+        };
+
+        // setting text color rebuilds layout, so don't do it if we don't have to
+        if !old_data.same(data) {
+            self.selection = self.selection.constrain_to(content);
+            self.text.set_text(data.as_str());
+            if data.is_empty() {
+                self.text.set_text_color(theme::PLACEHOLDER_COLOR);
+            } else {
+                self.text.set_text_color(theme::LABEL_COLOR);
+            }
+        }
+
+        self.text.rebuild_if_needed(&mut ctx.text(), env);
         ctx.request_paint();
     }
 
     fn layout(
         &mut self,
-        _layout_ctx: &mut LayoutCtx,
+        _ctx: &mut LayoutCtx,
         bc: &BoxConstraints,
         _data: &String,
         env: &Env,
     ) -> Size {
         let width = env.get(theme::WIDE_WIDGET_WIDTH);
-        let height = env.get(theme::BORDERED_WIDGET_HEIGHT);
+        let min_height = env.get(theme::BORDERED_WIDGET_HEIGHT);
+
+        let text_metrics = self.text.size();
+        let text_height = text_metrics.height + TEXT_INSETS.y_value();
+        let height = text_height.max(min_height);
 
         let size = bc.constrain((width, height));
         self.width = size.width;
@@ -405,7 +433,7 @@ impl Widget<String> for TextBox {
 
         self.selection = self.selection.constrain_to(content);
 
-        let height = env.get(theme::BORDERED_WIDGET_HEIGHT);
+        let height = ctx.size().height;
         let background_color = env.get(theme::BACKGROUND_LIGHT);
         let selection_color = env.get(theme::SELECTION_COLOR);
         let cursor_color = env.get(theme::CURSOR_COLOR);
@@ -431,36 +459,30 @@ impl Widget<String> for TextBox {
             rc.clip(clip_rect);
 
             // Calculate layout
-            let text_layout = self.get_layout(&mut rc.text(), &content, env, data.is_empty());
-            let text_size = text_layout.size();
+            let text_size = self.text.size();
 
             // Shift everything inside the clip by the hscroll_offset
             rc.transform(Affine::translate((-self.hscroll_offset, 0.)));
 
             // Layout, measure, and draw text
-            let top_padding = (height - text_size.height).min(PADDING_TOP).max(0.);
-            let text_pos = Point::new(PADDING_LEFT, top_padding);
+            let extra_padding = (height - text_size.height - TEXT_INSETS.y_value()).max(0.) / 2.;
+            let text_pos = Point::new(TEXT_INSETS.x0, TEXT_INSETS.y0 + extra_padding);
 
             // Draw selection rect
             if !self.selection.is_caret() {
-                for sel in text_layout.rects_for_range(self.selection.range()) {
-                    let sel = sel + Vec2::new(PADDING_LEFT, top_padding);
+                for sel in self.text.rects_for_range(self.selection.range()) {
+                    let sel = sel + text_pos.to_vec2();
                     let rounded = sel.to_rounded_rect(1.0);
                     rc.fill(rounded, &selection_color);
                 }
             }
 
-            rc.draw_text(&text_layout, text_pos);
+            self.text.draw(rc, text_pos);
 
             // Paint the cursor if focused and there's no selection
             if is_focused && self.cursor_on {
-                let pos = text_layout.hit_test_text_position(self.cursor());
-                let metrics = text_layout.line_metric(pos.line).unwrap();
-                //let cursor_x = self.x_for_offset(&text_layout, self.cursor());
-                let xy = text_pos + Vec2::new(pos.point.x, 0.0);
-                let x2y2 = xy + Vec2::new(0., metrics.height);
-                let line = Line::new(xy, x2y2);
-
+                let line = self.text.cursor_line_for_text_position(self.cursor());
+                let line = line + text_pos.to_vec2();
                 rc.stroke(line, &cursor_color, 1.);
             }
         });

commit 61e18a2799f4f04b2da51187bc68c305ccbd7990

Fix issues with placeholder TextBox text
This was broken slightly by #1182. This patch also cleans up a few
changes from that patch in the styled_text example that I had
not intended to commit.

As reported by @totsteps

druid/examples/styled_text.rs
@@ -118,7 +118,7 @@ fn ui_builder() -> impl Widget<AppData> {
     let mono_checkbox = Checkbox::new("Monospace").lens(AppData::mono);
 
     let input = TextBox::new()
-        .with_text_size(38.0)
+        .with_placeholder("Your sample text here :)")
         .fix_width(200.0)
         .lens(AppData::text);
 
@@ -133,5 +133,4 @@ fn ui_builder() -> impl Widget<AppData> {
         .with_child(mono_checkbox)
         .with_spacer(8.0)
         .with_child(input.padding(5.0))
-        .debug_widget_id()
 }

druid/src/text/layout.rs
@@ -181,7 +181,7 @@ impl TextLayout {
                 let line_metrics = layout.line_metric(pos.line).unwrap();
                 let p1 = (pos.point.x, line_metrics.y_offset);
                 let p2 = (pos.point.x, (line_metrics.y_offset + line_metrics.height));
-                dbg!(Line::new(p1, p2))
+                Line::new(p1, p2)
             })
             .unwrap_or_else(|| Line::new(Point::ZERO, Point::ZERO))
     }

druid/src/widget/textbox.rs
@@ -360,7 +360,11 @@ impl Widget<String> for TextBox {
 
             self.do_edit_action(edit_action, data);
             self.reset_cursor_blink(ctx);
-            self.text.set_text(data.as_str());
+            if data.is_empty() {
+                self.text.set_text(self.placeholder.as_str());
+            } else {
+                self.text.set_text(data.as_str());
+            }
             self.text.rebuild_if_needed(ctx.text(), env);
 
             if !is_select_all {
@@ -373,7 +377,12 @@ impl Widget<String> for TextBox {
         match event {
             LifeCycle::WidgetAdded => {
                 ctx.register_for_focus();
-                self.text.set_text(data.clone());
+                if data.is_empty() {
+                    self.text.set_text(self.placeholder.as_str());
+                    self.text.set_text_color(theme::PLACEHOLDER_COLOR);
+                } else {
+                    self.text.set_text(data.as_str());
+                }
                 self.text.rebuild_if_needed(ctx.text(), env);
             }
             // an open question: should we be able to schedule timers here?