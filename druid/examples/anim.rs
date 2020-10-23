253c3f358ff500c1c17aa24b2fc1670f17bc21e2
jaap aarts <jaap.aarts1@gmail.com>
2020-10-23 12:22:39 +0200

    add comment

--- druid/examples/anim.rs
+++ druid/examples/anim.rs
@@ -44,6 +44,9 @@ impl Widget<()> for AnimWidget {
                 if self.t < 1.0 {
                     ctx.request_anim_frame();
                 } else {
+                    // We might have t>1.0 as the end of the animation,
+                    // we want to make sure the line points up at the
+                    // end of the animation.
                     self.t = 0.0;
                 }
             }
