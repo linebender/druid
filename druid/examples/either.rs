a56a4076af24f615fe60dd3f00c7484434eab8a7
jaap aarts <jaap.aarts1@gmail.com>
2020-11-04 14:53:39 +0100

    add docs and simplify

--- druid/examples/either.rs
+++ druid/examples/either.rs
@@ -12,8 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! An example using the Either widget to show/hide a slider.
+//! This is a very simple example, it uses a bool to determine
+//! which widget gets shown.
+
+use druid::widget::prelude::*;
 use druid::widget::{Checkbox, Either, Flex, Label, Slider};
-use druid::{AppLauncher, Data, Lens, LocalizedString, Widget, WidgetExt, WindowDesc};
+use druid::{AppLauncher, Data, Lens, WidgetExt, WindowDesc};
 
 #[derive(Clone, Default, Data, Lens)]
 struct AppState {
@@ -22,32 +27,28 @@ struct AppState {
 }
 
 pub fn main() {
-    let main_window = WindowDesc::new(ui_builder).title(
-        LocalizedString::new("either-demo-window-title")
-            .with_placeholder("Switcheroo")
-            .with_arg("view", |data: &AppState, _env| (data.which as u8).into()),
-    );
-    let data = AppState::default();
+    let main_window =
+        WindowDesc::new(ui_builder).title(druid::widget::LabelText::from("Switcheroo"));
     AppLauncher::with_window(main_window)
         .use_simple_logger()
-        .launch(data)
+        .launch(AppState::default())
         .expect("launch failed");
 }
 
 fn ui_builder() -> impl Widget<AppState> {
-    let label = Label::new("Click to reveal slider");
+    // Our UI consists of a column with a button and an `Either` widget
+    let button = Checkbox::new("Toggle slider")
+        .lens(AppState::which)
+        .padding(5.0);
 
-    let mut col = Flex::column();
-    col.add_child(
-        Checkbox::new("Toggle slider")
-            .lens(AppState::which)
-            .padding(5.0),
-    );
+    // The either widget has 2 children, one of which is visible.
+    // you have to pass in a closure which takes `Data` and and `Env`.
+    // This closure determines which widget gets displayed based on the
+    // return value (a bool). False is the second widget and true is the first.
     let either = Either::new(
         |data, _env| data.which,
         Slider::new().lens(AppState::value).padding(5.0),
-        label.padding(5.0),
+        Label::new("Click to reveal slider").padding(5.0),
     );
-    col.add_child(either);
-    col
+    Flex::column().with_child(button).with_child(either)
 }
