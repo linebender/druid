8c57924b087518b8b5344fde3bbf4cd9dadeec01
jaap aarts <jaap.aarts1@gmail.com>
2020-11-07 16:44:06 +0100

    final rework

--- druid/examples/event_viewer.rs
+++ druid/examples/event_viewer.rs
@@ -15,21 +15,19 @@
 //! An application that accepts keyboard and mouse input, and displays
 //! information about received events.
 
-use druid::widget::{
-    prelude::*, Controller, CrossAxisAlignment, Flex, Label, List, Scroll, SizedBox, TextBox,
-};
+use druid::widget::prelude::*;
+use druid::widget::{Controller, CrossAxisAlignment, Flex, Label, List, Scroll, SizedBox, TextBox};
 use druid::{
-    theme, AppLauncher, Color, Data, Env, FontDescriptor, KeyEvent, Lens, LocalizedString,
-    Location, Modifiers, MouseButton, MouseEvent, Widget, WidgetExt, WindowDesc,
+    theme, AppLauncher, Color, Data, FontDescriptor, KeyEvent, Lens, Location, Modifiers,
+    MouseButton, MouseEvent, WidgetExt, WindowDesc,
 };
 use std::sync::Arc;
 
-const WINDOW_TITLE: LocalizedString<AppState> = LocalizedString::new("Event Viewer");
-const INACTIVE_AREA_COLOR: Color = Color::grey8(0x55);
+const CURSOR_BACKGROUND_COLOR: Color = Color::grey8(0x55);
 const HEADER_BACKGROUND: Color = Color::grey8(0xCC);
-const COLUMN_PADDING: f64 = 2.0;
 const INTERACTIVE_AREA_DIM: f64 = 160.0;
-
+const INTERACTIVE_AREA_BORDER: Color = Color::grey8(0xCC);
+const TEXT_COLOR: Color = Color::grey8(0x11);
 const PROPERTIES: &[(&str, f64)] = &[
     ("#", 40.0),
     ("Event", 80.0),
@@ -47,122 +45,33 @@ const PROPERTIES: &[(&str, f64)] = &[
 struct AppState {
     /// The text in the text field
     text_input: String,
-    events: Arc<Vec<EventLog>>,
+    events: Arc<Vec<LoggedEvent>>,
 }
 
-fn build_root_widget() -> impl Widget<AppState> {
-    Flex::column()
-        .with_child(interactive_area())
-        .with_flex_child(event_list(), 1.0)
+struct EventLogger<F: Fn(&Event) -> bool> {
+    filter: F,
 }
 
-/// The top part of the application, that accepts keyboard and mouse input.
-fn interactive_area() -> impl Widget<AppState> {
-    let text_box = TextBox::multiline()
-        .with_text_color(Color::rgb8(0xf0, 0xf0, 0xea))
-        .lens(AppState::text_input)
-        .fix_size(INTERACTIVE_AREA_DIM, INTERACTIVE_AREA_DIM)
-        .controller(EventLogger {
-            filter: |event| matches!(event, Event::KeyDown(_) | Event::KeyUp(_)),
-        });
-
-    let mouse_box = SizedBox::empty()
-        .fix_size(INTERACTIVE_AREA_DIM, INTERACTIVE_AREA_DIM)
-        .background(INACTIVE_AREA_COLOR)
-        .rounded(5.0)
-        .border(Color::grey8(0xCC), 1.0)
-        .controller(EventLogger {
-            filter: |event| matches!(event, Event::MouseDown(_) | Event::MouseUp(_) | Event::Wheel(_)),
-		});
-
-    Flex::row()
-        .with_flex_spacer(1.0)
-        .with_child(text_box)
-        .with_flex_spacer(1.0)
-        .with_child(mouse_box)
-        .with_flex_spacer(1.0)
-        .padding(10.0)
-}
-
-/// The bottom part of the application, a list of received events.
-fn event_list() -> impl Widget<AppState> {
-    // Because this would be a HUGE block of repeated code with constants
-    // we just use a look to generate the header.
-    let mut header = Flex::row().with_child(
-        Label::new(PROPERTIES[0].0)
-            .fix_width(PROPERTIES[0].1)
-            .background(HEADER_BACKGROUND),
-    );
-
-    for (name, size) in PROPERTIES.into_iter().skip(1) {
-        header.add_default_spacer();
-        header.add_child(
-            Label::new(*name)
-                .fix_width(*size)
-                .background(HEADER_BACKGROUND),
-        );
+impl<W: Widget<AppState>, F: Fn(&Event) -> bool> Controller<AppState, W> for EventLogger<F> {
+    fn event(
+        &mut self,
+        child: &mut W,
+        ctx: &mut EventCtx,
+        event: &Event,
+        data: &mut AppState,
+        env: &Env,
+    ) {
+        // Every time this controller receives an event we check `f()`.
+        // If `f()` returns true it means that we can add it to the log,
+        // if not then we can skip it.
+        if (self.filter)(event) {
+            if let Some(to_log) = LoggedEvent::try_from_event(event, data.events.len()) {
+                Arc::make_mut(&mut data.events).push(to_log);
+            }
+        }
+        // Always pass on the event!
+        child.event(ctx, event, data, env)
     }
-    Scroll::new(
-        Flex::column()
-            .cross_axis_alignment(CrossAxisAlignment::Start)
-            .with_child(header)
-            .with_default_spacer()
-            .with_flex_child(
-                Scroll::new(List::new(make_list_item).lens(AppState::events)).vertical(),
-                1.0,
-            )
-            .background(Color::WHITE),
-    )
-    .horizontal()
-    .padding(10.0)
-}
-
-/// A single event row.
-fn make_list_item() -> impl Widget<EventLog> {
-    Flex::row()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.number()).fix_width(PROPERTIES[0].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.name()).fix_width(PROPERTIES[1].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.mouse_pos()).fix_width(PROPERTIES[2].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.wheel_delta()).fix_width(PROPERTIES[3].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.mouse_button()).fix_width(PROPERTIES[4].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.click_count()).fix_width(PROPERTIES[5].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.is_repeat()).fix_width(PROPERTIES[6].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.key()).fix_width(PROPERTIES[7].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.code()).fix_width(PROPERTIES[8].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.modifiers()).fix_width(PROPERTIES[9].1))
-        .with_default_spacer()
-        .with_child(Label::dynamic(|d: &EventLog, _| d.location()).fix_width(PROPERTIES[10].1))
-}
-
-pub fn main() {
-    //describe the main window
-    let main_window = WindowDesc::new(build_root_widget)
-        .title(WINDOW_TITLE)
-        .window_size((760.0, 680.0));
-
-    //start the application
-    AppLauncher::with_window(main_window)
-        .use_simple_logger()
-        .configure_env(|env, _| {
-            env.set(theme::UI_FONT, FontDescriptor::default().with_size(12.0));
-            env.set(theme::LABEL_COLOR, Color::grey8(0x11));
-            env.set(theme::WIDGET_PADDING_HORIZONTAL, COLUMN_PADDING);
-            env.set(theme::WIDGET_PADDING_VERTICAL, COLUMN_PADDING);
-        })
-        .launch(AppState {
-            text_input: String::new(),
-            events: Arc::new(Vec::new()),
-        })
-        .expect("Failed to launch application");
 }
 
 /// The types of events we display
@@ -175,9 +84,9 @@ enum EventType {
     Wheel,
 }
 
-/// A type that represents any logged event.
+/// A type that represents any logged event shown in the list
 #[derive(Clone, Data)]
-struct EventLog {
+struct LoggedEvent {
     typ: EventType,
     number: usize,
     // To see what #[data(ignore)] does look at the docs.rs page on `Data`:
@@ -188,7 +97,11 @@ struct EventLog {
     key: Option<KeyEvent>,
 }
 
-impl EventLog {
+/// Here we implement all the display elements of the log entry.
+/// We have one method for every attribute we want to show.
+/// This is not very interesting it is mostly just getting the data
+/// from the events and handling `None` values.
+impl LoggedEvent {
     fn try_from_event(event: &Event, number: usize) -> Option<Self> {
         let to_log = match event {
             Event::MouseUp(mouse) => Some((EventType::MouseUp, Some(mouse.clone()), None)),
@@ -199,13 +112,14 @@ impl EventLog {
             _ => None,
         };
 
-        to_log.map(|(typ, mouse, key)| EventLog {
+        to_log.map(|(typ, mouse, key)| LoggedEvent {
             typ,
             number,
             mouse,
             key,
         })
     }
+
     fn number(&self) -> String {
         self.number.to_string()
     }
@@ -218,7 +132,7 @@ impl EventLog {
             EventType::MouseUp => "MouseUp",
             EventType::Wheel => "Wheel",
         }
-        .to_string()
+        .into()
     }
 
     fn mouse_pos(&self) -> String {
@@ -302,29 +216,128 @@ impl EventLog {
     }
 }
 
-struct EventLogger<F: Fn(&Event) -> bool> {
-    filter: F,
+fn build_root_widget() -> impl Widget<AppState> {
+    Flex::column()
+        .with_child(interactive_area())
+        .with_flex_child(event_list(), 1.0)
 }
 
-impl<W: Widget<AppState>, F: Fn(&Event) -> bool> Controller<AppState, W> for EventLogger<F> {
-    fn event(
-        &mut self,
-        child: &mut W,
-        ctx: &mut EventCtx,
-        event: &Event,
-        data: &mut AppState,
-        env: &Env,
-    ) {
-        // Every time this controller receives an event we check if f() returns
-        // true. If it does this means that we can add it to the log.
-        if (self.filter)(event) {
-            if let Some(to_log) = EventLog::try_from_event(event, data.events.len()) {
-                Arc::make_mut(&mut data.events).push(to_log);
-            }
-        }
-        // Always pass on the event!
-        child.event(ctx, event, data, env)
+/// The top part of the application, that accepts keyboard and mouse input.
+fn interactive_area() -> impl Widget<AppState> {
+    let text_box = TextBox::multiline()
+        .with_text_color(Color::rgb8(0xf0, 0xf0, 0xea))
+        .fix_size(INTERACTIVE_AREA_DIM, INTERACTIVE_AREA_DIM)
+        .lens(AppState::text_input)
+        .controller(EventLogger {
+            filter: |event| matches!(event, Event::KeyDown(_) | Event::KeyUp(_)),
+        });
+
+    let mouse_box = SizedBox::empty()
+        .fix_size(INTERACTIVE_AREA_DIM, INTERACTIVE_AREA_DIM)
+        .background(CURSOR_BACKGROUND_COLOR)
+        .rounded(5.0)
+        .border(INTERACTIVE_AREA_BORDER, 1.0)
+        .controller(EventLogger {
+            filter: |event| matches!(event, Event::MouseDown(_) | Event::MouseUp(_) | Event::Wheel(_)),
+		});
+
+    Flex::row()
+        .with_flex_spacer(1.0)
+        .with_child(text_box)
+        .with_flex_spacer(1.0)
+        .with_child(mouse_box)
+        .with_flex_spacer(1.0)
+        .padding(10.0)
+}
+
+/// The bottom part of the application, a list of received events.
+fn event_list() -> impl Widget<AppState> {
+    // Because this would be a HUGE block of repeated code with constants
+    // we just use a look to generate the header.
+    let mut header = Flex::row().with_child(
+        Label::new(PROPERTIES[0].0)
+            .fix_width(PROPERTIES[0].1)
+            .background(HEADER_BACKGROUND),
+    );
+
+    for (name, size) in PROPERTIES.into_iter().skip(1) {
+        // Keep in mind that later on, in the main function,
+        // we set the default spacer values. Without explicitly
+        // setting them the default spacer is bigger, and is
+        // probably not desirable for your porpoises.
+        header.add_default_spacer();
+        header.add_child(
+            Label::new(*name)
+                .fix_width(*size)
+                .background(HEADER_BACKGROUND),
+        );
     }
+    Scroll::new(
+        Flex::column()
+            .cross_axis_alignment(CrossAxisAlignment::Start)
+            .with_child(header)
+            .with_default_spacer()
+            .with_flex_child(
+                // `List::new` generates a list entry for every element in the `Vec`.
+                // In this case it shows a log entry for every element in `AppState::events`.
+                // `make_list_item` generates this new log entry.
+                Scroll::new(List::new(make_list_item).lens(AppState::events)).vertical(),
+                1.0,
+            )
+            .background(Color::WHITE),
+    )
+    .horizontal()
+    .padding(10.0)
+}
+
+/// A single event row.
+fn make_list_item() -> impl Widget<LoggedEvent> {
+    Flex::row()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.number()).fix_width(PROPERTIES[0].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.name()).fix_width(PROPERTIES[1].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.mouse_pos()).fix_width(PROPERTIES[2].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.wheel_delta()).fix_width(PROPERTIES[3].1))
+        .with_default_spacer()
+        .with_child(
+            Label::dynamic(|d: &LoggedEvent, _| d.mouse_button()).fix_width(PROPERTIES[4].1),
+        )
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.click_count()).fix_width(PROPERTIES[5].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.is_repeat()).fix_width(PROPERTIES[6].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.key()).fix_width(PROPERTIES[7].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.code()).fix_width(PROPERTIES[8].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.modifiers()).fix_width(PROPERTIES[9].1))
+        .with_default_spacer()
+        .with_child(Label::dynamic(|d: &LoggedEvent, _| d.location()).fix_width(PROPERTIES[10].1))
+}
+
+pub fn main() {
+    //describe the main window
+    let main_window = WindowDesc::new(build_root_widget)
+        .title("Event Viewer")
+        .window_size((760.0, 680.0));
+
+    //start the application
+    AppLauncher::with_window(main_window)
+        .use_simple_logger()
+        .configure_env(|env, _| {
+            env.set(theme::UI_FONT, FontDescriptor::default().with_size(12.0));
+            env.set(theme::LABEL_COLOR, TEXT_COLOR);
+            env.set(theme::WIDGET_PADDING_HORIZONTAL, 2.0);
+            env.set(theme::WIDGET_PADDING_VERTICAL, 2.0);
+        })
+        .launch(AppState {
+            text_input: String::new(),
+            events: Arc::new(Vec::new()),
+        })
+        .expect("Failed to launch application");
 }
 
 fn modifiers_string(mods: Modifiers) -> String {
