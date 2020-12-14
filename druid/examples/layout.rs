900a0ced88ef0431e34566a21ebbd4ee9e6500e6
Jaap Aarts <JAicewizard@users.noreply.github.com>
2020-12-14 15:45:40 +0100

    Add documentation to layout example (#1450)
    * add documentation to Layout
    
    * Apply suggestions from code review
    
    Co-authored-by: Colin Rofls <colin@cmyr.net>

--- druid/examples/layout.rs
+++ druid/examples/layout.rs
@@ -12,31 +12,41 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! This example shows how to construct a basic layout.
+//! This example shows how to construct a basic layout,
+//! using columns, rows, and loops, for repeated Widgets.
 
 use druid::widget::{Button, Flex, Label};
-use druid::{AppLauncher, Color, LocalizedString, Widget, WidgetExt, WindowDesc};
+use druid::{AppLauncher, Color, Widget, WidgetExt, WindowDesc};
 
 fn build_app() -> impl Widget<u32> {
-    // Begin construction of vertical layout
-    let mut col = Flex::column();
-
-    // Construct a horizontal layout.
-    let mut header = Flex::row();
-    header.add_child(
-        Label::new("One")
-            .fix_width(60.0)
-            .background(Color::rgb8(0x77, 0x77, 0))
-            .border(Color::WHITE, 3.0)
-            .center(),
-    );
-    // Spacing element that will fill all available space in between label
-    // and a button. Notice that weight is non-zero.
-    header.add_flex_spacer(1.0);
-    header.add_child(Button::new("Two").padding(20.));
-    col.add_child(
-        header
+    // Usually we put all the widgets in one big tree using builder-style
+    // methods. Sometimes we split them up in declarations to increase
+    // readability. In this case we also have some recurring elements,
+    // we add those in a loop later on.
+    let mut col = Flex::column().with_child(
+        // The `Flex`'s first child is another Flex! In this case it is
+        // a row.
+        Flex::row()
+            // The row has its own children.
+            .with_child(
+                Label::new("One")
+                    .fix_width(60.0)
+                    .background(Color::rgb8(0x77, 0x77, 0))
+                    .border(Color::WHITE, 3.0)
+                    .center(),
+            )
+            // Spacing element that will fill all available space in
+            // between label and a button. Notice that weight is non-zero.
+            // We could have achieved a similair result with expanding the
+            // width and setting the main-axis-allignment to SpaceBetween.
+            .with_flex_spacer(1.0)
+            .with_child(Button::new("Two").padding(20.))
+            // After we added all the children, we can set some more
+            // values using builder-style methods. Since these methods
+            // dont return the original `Flex` but a SizedBox and Container
+            // respectively, we have to put these at the end.
             .fix_height(100.0)
+            //turquoise
             .background(Color::rgb8(0, 0x77, 0x88)),
     );
 
@@ -50,15 +60,16 @@ fn build_app() -> impl Widget<u32> {
             weight,
         );
     }
-
+    // This method asks druid to draw colored rectangles around our widgets,
+    // so we can visually inspect their layout rectangles.
     col.debug_paint_layout()
 }
 
 pub fn main() {
-    let window = WindowDesc::new(build_app)
-        .title(LocalizedString::new("layout-demo-window-title").with_placeholder("Very flexible"));
+    let window = WindowDesc::new(build_app).title("Very flexible");
+
     AppLauncher::with_window(window)
         .use_simple_logger()
-        .launch(0u32)
+        .launch(0)
         .expect("launch failed");
 }
