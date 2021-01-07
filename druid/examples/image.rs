c2e4c8aaf26dc0e1b7bccf41e8206a76db071f77
jaap aarts <jaap.aarts1@gmail.com>
2021-01-07 17:37:04 +0100

    completely refo image

--- druid/examples/image.rs
+++ druid/examples/image.rs
@@ -12,54 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//! This example shows how to draw an image; using images requires
-//! enabling the `image` feature in your Cargo.toml; you will also need
-//! to specify the image formats you would like to use. See druid/Cargo.toml
-//! to see all features.
-
-/* use druid::piet::InterpolationMode;
-use druid::widget::prelude::*;
-use druid::widget::{FillStrat, Flex, Image, WidgetExt};
-use druid::{AppLauncher, Color, ImageBuf, WindowDesc};
-
-pub fn main() {
-    let main_window = WindowDesc::new(ui_builder);
-    AppLauncher::with_window(main_window)
-        .use_simple_logger()
-        .launch(0)
-        .expect("launch failed");
-}
-
-fn ui_builder() -> impl Widget<u32> {
-    let png_data = ImageBuf::from_data(include_bytes!("./assets/PicWithAlpha.png")).unwrap();
-
-    // We create two images, one not having any modifications and the other
-    // forced to a fixed width, a fill strategy and an interpolation mode.
-    // You can see how this affects the final result. You can play with the
-    // interpolation mode to see how this affects things. (Note that this image
-    // is already anti-aliased so NearestNeighbor looks weird.)
-    Flex::column()
-        .with_flex_child(
-            Image::new(png_data.clone())
-                .fill_mode(FillStrat::FitWidth)
-                .interpolation_mode(InterpolationMode::NearestNeighbor)
-                .border(Color::WHITE, 1.0)
-                .fix_width(150.0)
-                .center(),
-            1.0,
-        )
-        .with_flex_child(
-            Image::new(png_data)
-                .border(Color::WHITE, 1.0),
-            1.0,
-        )
-}
- */
+//! This showcase demonstrates how to use the image widget and is
+//! propperties. You can change the parameters in the GUI to see how
+//! everything behaves.
 
 use druid::piet::InterpolationMode;
 use druid::widget::{prelude::*, FillStrat, Image};
 use druid::widget::{
-    Checkbox, CrossAxisAlignment, Flex, Label, RadioGroup, SizedBox, Stepper, TextBox, WidgetExt,
+    Checkbox, CrossAxisAlignment, Flex, Label, RadioGroup, SizedBox, TextBox, WidgetExt,
 };
 use druid::{AppLauncher, Color, Data, ImageBuf, Lens, LensExt, WindowDesc};
 
@@ -83,23 +43,8 @@ const INTERPOLATION_MODE_OPTIONS: [(&str, InterpolationModeData); 2] = [
         InterpolationModeData::new(InterpolationMode::NearestNeighbor),
     ),
 ];
-
 #[derive(Clone, Data, Lens)]
 struct AppState {
-    demo_state: DemoState,
-    params: Params,
-}
-
-#[derive(Clone, Data, Lens)]
-struct DemoState {
-    pub input_text: String,
-    pub enabled: bool,
-    volume: f64,
-}
-
-#[derive(Clone, Data, Lens)]
-struct Params {
-    debug_layout: bool,
     fill_strat: FillStrat,
     interpolate: bool,
     interpolation_mode: InterpolationModeData,
@@ -136,7 +81,7 @@ impl Rebuilder {
     }
 
     fn rebuild_inner(&mut self, data: &AppState) {
-        self.inner = build_widget(&data.params);
+        self.inner = build_widget(&data);
     }
 }
 
@@ -152,12 +97,10 @@ impl Widget<AppState> for Rebuilder {
         self.inner.lifecycle(ctx, event, data, env)
     }
 
-    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &AppState, data: &AppState, env: &Env) {
-        if !old_data.params.same(&data.params) {
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &AppState, data: &AppState, _env: &Env) {
+        if !old_data.same(&data) {
             self.rebuild_inner(data);
             ctx.children_changed();
-        } else {
-            self.inner.update(ctx, old_data, data, env);
         }
     }
 
@@ -174,10 +117,6 @@ impl Widget<AppState> for Rebuilder {
     fn paint(&mut self, ctx: &mut PaintCtx, data: &AppState, env: &Env) {
         self.inner.paint(ctx, data, env)
     }
-
-    fn id(&self) -> Option<WidgetId> {
-        self.inner.id()
-    }
 }
 
 fn make_control_row() -> impl Widget<AppState> {
@@ -188,7 +127,7 @@ fn make_control_row() -> impl Widget<AppState> {
                 .cross_axis_alignment(CrossAxisAlignment::Start)
                 .with_child(Label::new("FillStrat:"))
                 .with_default_spacer()
-                .with_child(RadioGroup::new(FILL_STRAT_OPTIONS.to_vec()).lens(Params::fill_strat)),
+                .with_child(RadioGroup::new(FILL_STRAT_OPTIONS.to_vec()).lens(AppState::fill_strat)),
         )
         .with_default_spacer()
         .with_child(
@@ -196,11 +135,9 @@ fn make_control_row() -> impl Widget<AppState> {
                 .cross_axis_alignment(CrossAxisAlignment::Start)
                 .with_child(Label::new("interpolation mode:"))
                 .with_default_spacer()
-                .with_child(Checkbox::new("set interpolation mode").lens(Params::interpolate))
-                .with_default_spacer()
                 .with_child(
                     RadioGroup::new(INTERPOLATION_MODE_OPTIONS.to_vec())
-                        .lens(Params::interpolation_mode),
+                        .lens(AppState::interpolation_mode),
                 ),
         )
         .with_default_spacer()
@@ -213,64 +150,47 @@ fn make_control_row() -> impl Widget<AppState> {
                 .cross_axis_alignment(CrossAxisAlignment::Start)
                 .with_child(Label::new("Misc:"))
                 .with_default_spacer()
-                .with_child(Checkbox::new("Debug layout").lens(Params::debug_layout))
+                .with_child(Checkbox::new("Fix width").lens(AppState::fix_width))
                 .with_default_spacer()
-                .with_child(Checkbox::new("Fix width").lens(Params::fix_width))
+                .with_child(Checkbox::new("Fix height").lens(AppState::fix_height))
                 .with_default_spacer()
-                .with_child(Checkbox::new("Fix height").lens(Params::fix_height)),
+                .with_child(Checkbox::new("set interpolation mode").lens(AppState::interpolate)),
         )
         .padding(10.0)
         .border(Color::grey(0.6), 2.0)
         .rounded(5.0)
-        .lens(AppState::params)
 }
 
-fn make_width() -> impl Widget<Params> {
+fn make_width() -> impl Widget<AppState> {
     Flex::column()
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .with_child(Label::new("width:"))
         .with_default_spacer()
         .with_child(
-            Flex::row()
-                .with_child(
-                    TextBox::new()
-                        .parse()
-                        .lens(Params::width.map(|x| Some(*x), |x, y| *x = y.unwrap_or_default()))
-                        .fix_width(60.0),
-                )
-                .with_spacer(druid::theme::WIDGET_CONTROL_COMPONENT_PADDING)
-                .with_child(
-                    Stepper::new()
-                        .with_range(2.0, 50.0)
-                        .with_step(2.0)
-                        .lens(Params::width),
-                ),
+            Flex::row().with_child(
+                TextBox::new()
+                    .parse()
+                    .lens(AppState::width.map(|x| Some(*x), |x, y| *x = y.unwrap_or_default()))
+                    .fix_width(60.0),
+            ),
         )
 }
-fn make_height() -> impl Widget<Params> {
+fn make_height() -> impl Widget<AppState> {
     Flex::column()
         .cross_axis_alignment(CrossAxisAlignment::Start)
         .with_child(Label::new("height:"))
         .with_default_spacer()
         .with_child(
-            Flex::row()
-                .with_child(
-                    TextBox::new()
-                        .parse()
-                        .lens(Params::height.map(|x| Some(*x), |x, y| *x = y.unwrap_or_default()))
-                        .fix_width(60.0),
-                )
-                .with_spacer(druid::theme::WIDGET_CONTROL_COMPONENT_PADDING)
-                .with_child(
-                    Stepper::new()
-                        .with_range(2.0, 50.0)
-                        .with_step(2.0)
-                        .lens(Params::height),
-                ),
+            Flex::row().with_child(
+                TextBox::new()
+                    .parse()
+                    .lens(AppState::height.map(|x| Some(*x), |x, y| *x = y.unwrap_or_default()))
+                    .fix_width(60.0),
+            ),
         )
 }
 
-fn build_widget(state: &Params) -> Box<dyn Widget<AppState>> {
+fn build_widget(state: &AppState) -> Box<dyn Widget<AppState>> {
     let png_data = ImageBuf::from_data(include_bytes!("./assets/PicWithAlpha.png")).unwrap();
 
     let mut img = Image::new(png_data.clone()).fill_mode(state.fill_strat);
@@ -284,11 +204,7 @@ fn build_widget(state: &Params) -> Box<dyn Widget<AppState>> {
     if state.fix_height {
         sized = sized.fix_height(state.height)
     }
-    if state.debug_layout {
-        sized.center().debug_paint_layout().boxed()
-    } else {
-        sized.center().boxed()
-    }
+    sized.border(Color::grey(0.6), 2.0).center().boxed()
 }
 
 fn make_ui() -> impl Widget<AppState> {
@@ -302,29 +218,21 @@ fn make_ui() -> impl Widget<AppState> {
 
 pub fn main() {
     let main_window = WindowDesc::new(make_ui)
-        .window_size((720., 600.))
-        .with_min_size((620., 300.))
+        .window_size((650., 450.))
         .title("Flex Container Options");
 
-    let demo_state = DemoState {
-        input_text: "hello".into(),
-        enabled: false,
-        volume: 0.0,
-    };
-
-    let params = Params {
-        debug_layout: false,
-        fill_strat: FillStrat::None,
-        interpolate: false,
+    let state = AppState {
+        fill_strat: FillStrat::Cover,
+        interpolate: true,
         interpolation_mode: InterpolationModeData::new(InterpolationMode::Bilinear),
-        fix_width: false,
-        width: 0.0,
-        fix_height: false,
-        height: 0.0,
+        fix_width: true,
+        width: 200.,
+        fix_height: true,
+        height: 100.,
     };
 
     AppLauncher::with_window(main_window)
         .use_simple_logger()
-        .launch(AppState { demo_state, params })
+        .launch(state)
         .expect("Failed to launch application");
 }
