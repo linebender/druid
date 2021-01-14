2140fbea858f9c31e764a6420880600b197ace6b
Robert Wittams <robert@wittams.com>
2021-01-14 14:02:30 +0000

    Update druid-shell/src/window.rs
    doc fix
    
    Co-authored-by: Colin Rofls <colin@cmyr.net>

--- druid-shell/src/window.rs
+++ druid-shell/src/window.rs
@@ -201,6 +201,7 @@ impl WindowHandle {
     }
 
     /// Returns the insets of the window content from its position and size in [pixels](crate::Scale).
+    ///
     /// This is to account for any window system provided chrome, eg. title bars.
     pub fn get_content_insets(&self) -> Insets {
         self.0.get_content_insets()
