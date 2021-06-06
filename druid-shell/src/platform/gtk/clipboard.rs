7953ca1ba5bd3cd954b45f62fdaa9392cc598f6e
Uli Schlachter <psychon@znc.in>
2021-06-06 17:41:37 +0200

    gtk: Replace unsound std::str::from_utf8_unchecked
    When putting text data into the clipboard, the platform code was calling
    std::str::from_utf8_unchecked() on the data. That's not really valid,
    because the caller can provide arbitrary bytes. Thus, this commit
    replaces that with a call to std::str::from_utf8().
    
    In case invalid utf8 was provided, the selection is instead set as raw
    bytes. I am not sure what will happen "down the road" with this, but it
    seems to be the best that can be done.
    
    Fixes: https://github.com/linebender/druid/issues/1814
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- druid-shell/src/platform/gtk/clipboard.rs
+++ druid-shell/src/platform/gtk/clipboard.rs
@@ -64,9 +64,17 @@ impl Clipboard {
             let idx = idx as usize;
             if idx < formats.len() {
                 let item = &formats[idx];
+                let mut not_set = true;
                 if item.identifier == ClipboardFormat::TEXT {
-                    sel.set_text(unsafe { std::str::from_utf8_unchecked(&item.data) });
-                } else {
+                    if let Ok(data) = std::str::from_utf8(&item.data) {
+                        sel.set_text(data);
+                        not_set = false;
+                    } else {
+                        tracing::warn!("Failed to put invalid utf8 text into the clipboard");
+                        // The code below will at least put it as "raw bytes"
+                    }
+                }
+                if not_set {
                     let atom = Atom::intern(item.identifier);
                     let stride = 8;
                     sel.set(&atom, stride, item.data.as_slice());
