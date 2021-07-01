0741915cfaf3614cd8c6c7a745f4330693f8bcfd
Maan2003 <manmeetmann2003@gmail.com>
2021-07-02 01:13:39 +0530

    allow deref_nullptr in tests

--- druid-shell/src/backend/x11/xkbcommon_sys.rs
+++ druid-shell/src/backend/x11/xkbcommon_sys.rs
@@ -1,4 +1,6 @@
 #![allow(unused, non_upper_case_globals, non_camel_case_types)]
+// unknown lints to make compile on older rust versions
+#![cfg_attr(test, allow(unknown_lints, deref_nullptr))]
 // generated code has some redudant static lifetimes, I don't think we can change that.
 #![allow(clippy::redundant_static_lifetimes)]
 
