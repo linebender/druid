0a933d5d6f47f56af1bd8d1039a70cd2ecb96071
Uli Schlachter <psychon@znc.in>
2021-07-03 19:04:14 +0200

    Please rustfmt
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- druid-shell/src/platform/x11/clipboard.rs
+++ druid-shell/src/platform/x11/clipboard.rs
@@ -75,11 +75,10 @@ impl Clipboard {
     }
 
     pub fn get_string(&self) -> Option<String> {
-        STRING_TARGETS
-            .iter()
-            .find_map(|target| self
-                 .get_format(target)
-                 .and_then(|data| String::from_utf8(data).ok()))
+        STRING_TARGETS.iter().find_map(|target| {
+            self.get_format(target)
+                .and_then(|data| String::from_utf8(data).ok())
+        })
     }
 
     pub fn preferred_format(&self, formats: &[FormatId]) -> Option<FormatId> {
