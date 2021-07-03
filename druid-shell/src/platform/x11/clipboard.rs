5bd277d29ff621b9c108f429abc11a03924c2393
Uli Schlachter <psychon@users.noreply.github.com>
2021-07-03 18:56:32 +0200

    Update druid-shell/src/platform/x11/clipboard.rs
    @Maan2003 turn `for` into functional!
    
    Co-authored-by: Manmeet Maan <49202620+Maan2003@users.noreply.github.com>

--- druid-shell/src/platform/x11/clipboard.rs
+++ druid-shell/src/platform/x11/clipboard.rs
@@ -75,12 +75,10 @@ impl Clipboard {
     }
 
     pub fn get_string(&self) -> Option<String> {
-        for target in STRING_TARGETS.iter() {
-            if let Some(data) = self.get_format(target) {
-                return String::from_utf8(data).ok();
-            }
-        }
-        None
+        STRING_TARGETS
+            .iter()
+            .find_map(|target| self.get_format(target))
+            .and_then(|data| String::from_utf8(data).ok())
     }
 
     pub fn preferred_format(&self, formats: &[FormatId]) -> Option<FormatId> {
