691c07d20974dee76f95702b9402a41e9e439611
Uli Schlachter <psychon@znc.in>
2021-07-04 10:20:30 +0200

    x11: Implement putting things into the clipboard
    Fixes: https://github.com/linebender/druid/issues/937
    Signed-off-by: Uli Schlachter <psychon@znc.in>

--- druid-shell/src/platform/x11/clipboard.rs
+++ druid-shell/src/platform/x11/clipboard.rs
@@ -16,20 +16,23 @@
 
 use std::cell::{Cell, RefCell};
 use std::collections::VecDeque;
+use std::convert::TryFrom;
 use std::rc::Rc;
 
-use x11rb::connection::Connection;
-use x11rb::errors::{ConnectionError, ReplyOrIdError};
+use x11rb::connection::{Connection, RequestConnection};
+use x11rb::errors::{ConnectionError, ReplyError, ReplyOrIdError};
 use x11rb::protocol::xproto::{
-    AtomEnum, ChangeWindowAttributesAux, ConnectionExt, EventMask, GetPropertyReply,
-    GetPropertyType, Property, PropertyNotifyEvent, SelectionClearEvent, SelectionRequestEvent,
-    Timestamp, WindowClass,
+    Atom, AtomEnum, ChangeWindowAttributesAux, ConnectionExt, EventMask, GetPropertyReply,
+    GetPropertyType, Property, PropertyNotifyEvent, PropMode, SelectionClearEvent,
+    SelectionNotifyEvent, SelectionRequestEvent, SELECTION_NOTIFY_EVENT, Timestamp, Window,
+    WindowClass,
 };
 use x11rb::protocol::Event;
+use x11rb::wrapper::ConnectionExt as _;
 use x11rb::xcb_ffi::XCBConnection;
 
 use crate::clipboard::{ClipboardFormat, FormatId};
-use tracing::{debug, warn};
+use tracing::{debug, error, warn};
 
 // We can pick an arbitrary atom that is used for the transfer. This is our pick.
 const TRANSFER_ATOM: AtomEnum = AtomEnum::CUT_BUFFE_R4;
@@ -76,11 +79,17 @@ impl Clipboard{
     }
 
     pub fn put_string(&mut self, s: impl AsRef<str>) {
-        self.0.borrow_mut().put_string(s.as_ref())
+        let bytes = s.as_ref().as_bytes();
+        let formats = STRING_TARGETS.iter()
+            .map(|format| ClipboardFormat::new(format, bytes))
+            .collect::<Vec<_>>();
+        self.put_formats(&formats);
     }
 
     pub fn put_formats(&mut self, formats: &[ClipboardFormat]) {
-        self.0.borrow_mut().put_formats(formats)
+        if let Err(err) = self.0.borrow_mut().put_formats(formats) {
+            error!("Error in Clipboard::put_formats: {:?}", err);
+        }
     }
 
     pub fn get_string(&self) -> Option<String> {
@@ -100,13 +109,15 @@ impl Clipboard{
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 struct ClipboardState {
     connection: Rc<XCBConnection>,
     screen_num: usize,
     atoms: ClipboardAtoms,
     event_queue: Rc<RefCell<VecDeque<Event>>>,
     timestamp: Rc<Cell<Timestamp>>,
+    contents: Option<ClipboardContents>,
+    incremental: Vec<IncrementalTransfer>,
 }
 
 impl ClipboardState {
@@ -123,19 +134,35 @@ impl ClipboardState {
             atoms,
             event_queue,
             timestamp,
+            contents: None,
+            incremental: Vec::new(),
         })
     }
 
-    fn put_string(&mut self, s: &str) {
-        let formats = STRING_TARGETS.iter()
-            .map(|format| ClipboardFormat::new(format, s.as_bytes()))
-            .collect::<Vec<_>>();
-        self.put_formats(&formats);
-    }
+    fn put_formats(&mut self, formats: &[ClipboardFormat]) -> Result<(), ReplyOrIdError> {
+        let conn = &*self.connection;
 
-    fn put_formats(&mut self, _formats: &[ClipboardFormat]) {
-        // TODO(x11/clipboard): implement Clipboard::put_formats
-        warn!("Clipboard::put_formats is currently unimplemented for X11 platforms.");
+        // Create a window for selection ownership and save the necessary state
+        let contents = ClipboardContents::new(conn, self.screen_num, formats)?;
+
+        // Become CLIPBOARD selection owner
+        conn.set_selection_owner(contents.owner_window, self.atoms.CLIPBOARD, self.timestamp.get())?;
+
+        // Check if we really are the selection owner; this might e.g. fail if our timestamp is too
+        // old and some other program became the selection owner with a newer timestamp
+        let owner = conn.get_selection_owner(self.atoms.CLIPBOARD)?.reply()?;
+        if owner.owner == contents.owner_window {
+            // We are the new selection owner! Remember our contents for later.
+            debug!("put_formats(): became selection owner");
+            if let Some(mut old_owner) = std::mem::replace(&mut self.contents, Some(contents)) {
+                // We already where the owner before. Destroy the old contents.
+                old_owner.destroy(conn)?;
+            }
+        } else {
+            debug!("put_formats(): failed to become selection owner");
+        }
+
+        Ok(())
     }
 
     fn get_string(&self) -> Option<String> {
@@ -154,11 +181,24 @@ impl ClipboardState {
     }
 
     fn get_format(&self, format: FormatId) -> Option<Vec<u8>> {
-        self.do_transfer(format, |prop| prop.value)
+        if let Some(contents) = self.contents.as_ref() {
+            // We are the selection owner and can directly return the result
+            contents.data.iter()
+                .find(|(_, fmt, _)| fmt == format)
+                .map(|(_, _, data)| data.to_vec())
+        } else {
+            self.do_transfer(format, |prop| prop.value)
+        }
     }
 
     #[allow(clippy::needless_collect)]
     fn available_type_names(&self) -> Vec<String> {
+        if let Some(contents) = self.contents.as_ref() {
+            // We are the selection owner and can directly return the result
+            return contents.data.iter()
+                .map(|(_, format, _)| format.to_string())
+                .collect();
+        }
         let requests = self
             .do_transfer("TARGETS", |prop| {
                 prop.value32()
@@ -224,6 +264,11 @@ impl ClipboardState {
                 Event::SelectionNotify(notify) if notify.requestor == window.window => {
                     break notify
                 }
+                Event::SelectionRequest(request) if request.requestor == window.window => {
+                    // The callers should catch this situation before and not even call us
+                    // do_transfer()
+                    error!("BUG! We are doing a selection transfer while we are the selection owner. This will hang!");
+                }
                 event => self.event_queue.borrow_mut().push_back(event),
             }
         };
@@ -287,18 +332,220 @@ impl ClipboardState {
         }
     }
 
-    fn handle_clear(&self, _event: &SelectionClearEvent) -> Result<(), ConnectionError> {
+    fn handle_clear(&mut self, event: &SelectionClearEvent) -> Result<(), ConnectionError> {
+        let window = self.contents.as_ref().map(|c| c.owner_window);
+        if Some(event.owner) == window {
+            // We lost ownership of the selection, clean up
+            if let Some(mut contents) = self.contents.take() {
+                contents.destroy(&*self.connection)?;
+            }
+        }
         Ok(())
     }
 
-    fn handle_request(&self, _event: &SelectionRequestEvent) -> Result<(), ReplyOrIdError> {
+    fn handle_request(&mut self, event: &SelectionRequestEvent) -> Result<(), ReplyOrIdError> {
+        let conn = &*self.connection;
+        let contents = match &self.contents {
+            Some(contents) if contents.owner_window == event.owner => contents,
+            _ => {
+                // We do not know what to do with this transfer
+                debug!("Got non-matching selection request event");
+                reject_transfer(conn, event)?;
+                return Ok(());
+            }
+        };
+
+        // TODO: ICCCM has TIMESTAMP as a required target (but no one uses it...?)
+        if event.target == self.atoms.TARGETS {
+            // TARGETS is a special case: reply is list of u32
+            let mut atoms = contents.data.iter()
+                .map(|(atom, _, _)| *atom)
+                .collect::<Vec<_>>();
+            atoms.push(self.atoms.TARGETS);
+            conn.change_property32(
+                PropMode::REPLACE,
+                event.requestor,
+                event.property,
+                AtomEnum::ATOM,
+                &atoms,
+            )?;
+        } else {
+            // Find the request target
+            let content = contents.data.iter()
+                .find(|(atom, _, _)| *atom == event.target);
+            match content {
+                None => {
+                    reject_transfer(conn, event)?;
+                    return Ok(());
+                }
+                Some((atom, _, data)) => {
+                    if data.len() > maximum_property_length(conn) {
+                        // We need to do an INCR transfer.
+                        debug!("Starting new INCR transfer");
+                        let transfer = IncrementalTransfer::new(
+                            conn,
+                            event,
+                            Rc::clone(&data),
+                            self.atoms.INCR,
+                        );
+                        match transfer {
+                            Ok(transfer) => self.incremental.push(transfer),
+                            Err(err) => {
+                                reject_transfer(conn, event)?;
+                                return Err(err.into());
+                            }
+                        }
+                    } else {
+                        // We can provide the data directly
+                        conn.change_property8(
+                            PropMode::REPLACE,
+                            event.requestor,
+                            event.property,
+                            *atom,
+                            data,
+                        )?;
+                    }
+                }
+            }
+        }
+
+        // Inform the requestor that we sent the data
+        debug!("Replying to selection request event");
+        let event = SelectionNotifyEvent {
+            response_type: SELECTION_NOTIFY_EVENT,
+            sequence: 0,
+            requestor: event.requestor,
+            selection: event.selection,
+            target: event.target,
+            property: event.property,
+            time: event.time,
+        };
+        conn.send_event(false, event.requestor, EventMask::NO_EVENT, &event)?;
+
         Ok(())
     }
 
-    fn handle_property_notify(&self, _event: &PropertyNotifyEvent) -> Result<(), ReplyOrIdError> {
+    fn handle_property_notify(&mut self, event: &PropertyNotifyEvent) -> Result<(), ReplyOrIdError> {
+        fn matches(transfer: &IncrementalTransfer, event: &PropertyNotifyEvent) -> bool {
+            transfer.requestor == event.window && transfer.property == event.atom
+        }
+
+        if event.state != Property::DELETE {
+            return Ok(());
+        }
+        // Deleting the target property indicates that an INCR transfer should continue. Find that
+        // transfer
+        if let Some(transfer) = self.incremental.iter_mut().find(|transfer| matches(transfer, event)) {
+            let done = transfer.continue_incremental(&*self.connection)?;
+            if done {
+                debug!("INCR transfer finished");
+                // Remove the transfer
+                self.incremental.retain(|transfer| !matches(transfer, event));
+            }
+        }
         Ok(())
     }
+}
 
+#[derive(Debug)]
+struct ClipboardContents {
+    owner_window: Window,
+    data: Vec<(Atom, String, Rc<Vec<u8>>)>,
+}
+
+impl ClipboardContents {
+    fn new(
+        conn: &XCBConnection,
+        screen_num: usize,
+        formats: &[ClipboardFormat],
+    ) -> Result<Self, ReplyOrIdError> {
+        // Send InternAtom requests for all formats
+        let data = formats.iter()
+            .map(|format| conn.intern_atom(false, format.identifier.as_bytes()).map(|cookie| (cookie, format)))
+            .collect::<Result<Vec<_>, ConnectionError>>()?;
+        // Get the replies for all InternAtom requests
+        let data = data.into_iter()
+            .map(|(cookie, format)| cookie.reply().map(|reply| (reply.atom, format.identifier.to_string(), Rc::new(format.data.clone()))))
+            .collect::<Result<Vec<_>, ReplyError>>()?;
+
+        let owner_window = conn.generate_id()?;
+        conn.create_window(
+            x11rb::COPY_DEPTH_FROM_PARENT,
+            owner_window,
+            conn.setup().roots[screen_num].root,
+            0,
+            0,
+            1,
+            1,
+            0,
+            WindowClass::INPUT_OUTPUT,
+            x11rb::COPY_FROM_PARENT,
+            &Default::default(),
+        )?;
+        Ok(Self {
+            owner_window,
+            data,
+        })
+    }
+
+    fn destroy(&mut self, conn: &XCBConnection) -> Result<(), ConnectionError> {
+        conn.destroy_window(std::mem::replace(&mut self.owner_window, x11rb::NONE))?;
+        Ok(())
+    }
+}
+
+#[derive(Debug)]
+struct IncrementalTransfer {
+    requestor: Window,
+    selection: Atom,
+    target: Atom,
+    property: Atom,
+    time: Timestamp,
+    data: Rc<Vec<u8>>,
+    data_offset: usize,
+}
+
+impl IncrementalTransfer {
+    fn new(conn: &XCBConnection, event: &SelectionRequestEvent, data: Rc<Vec<u8>>, incr: Atom) -> Result<Self, ConnectionError> {
+        // We need PropertyChange events on the window
+        conn.change_window_attributes(
+            event.requestor,
+            &ChangeWindowAttributesAux::new().event_mask(EventMask::PROPERTY_CHANGE),
+        )?;
+        // Indicate that we are doing an INCR transfer
+        let length = u32::try_from(data.len()).unwrap_or(u32::MAX);
+        conn.change_property32(
+            PropMode::REPLACE,
+            event.requestor,
+            event.property,
+            incr,
+            &[length],
+        )?;
+        Ok(Self {
+            requestor: event.requestor,
+            selection: event.selection,
+            target: event.target,
+            property: event.property,
+            time: event.time,
+            data,
+            data_offset: 0,
+        })
+    }
+
+    /// Continue an incremental transfer, returning true if the transfer is finished
+    fn continue_incremental(&mut self, conn: &XCBConnection) -> Result<bool, ConnectionError> {
+        let remaining = &self.data[self.data_offset..];
+        let next_length = remaining.len().min(maximum_property_length(conn));
+        conn.change_property8(
+            PropMode::REPLACE,
+            self.requestor,
+            self.property,
+            self.target,
+            &remaining[..next_length],
+        )?;
+        self.data_offset += next_length;
+        Ok(remaining.is_empty())
+    }
 }
 
 struct WindowContainer<'a> {
@@ -330,4 +577,25 @@ impl Drop for WindowContainer<'_> {
     fn drop(&mut self) {
         let _ = self.conn.destroy_window(self.window);
     }
+}
+
+fn maximum_property_length(connection: &XCBConnection) -> usize {
+    let change_property_header_size = 24;
+    // Apply an arbitraty limit to the property size to not stress the server too much
+    let max_request_length = connection.maximum_request_bytes().min(usize::from(u16::MAX));
+    max_request_length - change_property_header_size
+}
+
+fn reject_transfer(conn: &XCBConnection, event: &SelectionRequestEvent) -> Result<(), ConnectionError> {
+    let event = SelectionNotifyEvent {
+        response_type: SELECTION_NOTIFY_EVENT,
+        sequence: 0,
+        requestor: event.requestor,
+        selection: event.selection,
+        target: event.target,
+        property: x11rb::NONE,
+        time: event.time,
+    };
+    conn.send_event(false, event.requestor, EventMask::NO_EVENT, &event)?;
+    Ok(())
 }
