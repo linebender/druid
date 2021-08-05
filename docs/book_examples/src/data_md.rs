1c9f978eaca36e6c92a10c79b57b7eac8b050858
Olivier FAURE <couteaubleu@gmail.com>
2021-08-05 16:55:37 +0200

    Document `#[data(ignore)]` in book

--- docs/book_examples/src/data_md.rs
+++ docs/book_examples/src/data_md.rs
@@ -17,15 +17,22 @@ struct TodoList {
 /// A single todo item.
 struct TodoItem {
     category: Category,
+    title: String,
+    note: Option<String>,
+    completed: bool,
+
     // `Data` is implemented for any `Arc`.
     due_date: Option<Arc<DateTime>>,
+
     // You can specify a custom comparison fn
     // (anything with the signature (&T, &T) -> bool).
     #[data(same_fn = "PartialEq::eq")]
     added_date: DateTime,
-    title: String,
-    note: Option<String>,
-    completed: bool,
+
+    // You can specify that a field should
+    // be skipped when computing same-ness
+    #[data(ignore)]
+    debug_timestamp: usize,
 }
 
 #[derive(Clone, Data, PartialEq)]
