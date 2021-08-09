0535ec5bf6ca8e8977d2e515b0acf528060c6634
Maan2003 <manmeetmann2003@gmail.com>
2021-08-09 20:49:18 +0530

    RichText: Invalidate layout on Env change

--- druid/src/text/layout.rs
+++ druid/src/text/layout.rs
@@ -17,7 +17,7 @@
 use std::ops::Range;
 use std::rc::Rc;
 
-use super::{Link, TextStorage};
+use super::{EnvUpdateCtx, Link, TextStorage};
 use crate::kurbo::{Line, Point, Rect, Size};
 use crate::piet::{
     Color, PietText, PietTextLayout, Text as _, TextAlignment, TextAttribute, TextLayout as _,
@@ -347,6 +347,11 @@ impl<T: TextStorage> TextLayout<T> {
                     .text_size_override
                     .as_ref()
                     .map(|k| ctx.env_key_changed(k))
+                    .unwrap_or(false)
+                || self
+                    .text
+                    .as_ref()
+                    .map(|text| text.env_update(&EnvUpdateCtx::for_update(ctx)))
                     .unwrap_or(false);
 
             if rebuild {
