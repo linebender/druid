b3dccd2a69ae268a61ab15667eda0d116a1c3ccc
Christoph <xarvix@web.de>
2021-09-24 17:16:01 +0200

    changing id to kebab case
    Signed-off-by: Christoph <xarvix@web.de>

--- druid/src/command.rs
+++ druid/src/command.rs
@@ -342,7 +342,7 @@ pub mod sys {
     ///
     /// [`scroll_to_view`]: crate::EventCtx::scroll_to_view()
     /// [`scroll_area_to_view`]: crate::EventCtx::scroll_area_to_view()
-    pub const SCROLL_TO_VIEW: Selector<Rect> = Selector::new("druid_builtin.scroll_to");
+    pub const SCROLL_TO_VIEW: Selector<Rect> = Selector::new("druid-builtin.scroll-to");
 
     /// A change that has occured to text state, and needs to be
     /// communicated to the platform.
