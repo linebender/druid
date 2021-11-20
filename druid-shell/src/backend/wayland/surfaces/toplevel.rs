29a28449145a6d0563514b7016cb6fce39cf601c
James Lawrence <jljatone@gmail.com>
2021-11-20 05:42:45 -0500

    formatting

--- druid-shell/src/backend/wayland/surfaces/toplevel.rs
+++ druid-shell/src/backend/wayland/surfaces/toplevel.rs
@@ -6,12 +6,12 @@ use wayland_protocols::xdg_shell::client::xdg_toplevel;
 use crate::kurbo;
 use crate::window;
 
-use super::Outputs;
 use super::surface;
 use super::Compositor;
 use super::CompositorHandle;
 use super::Decor;
 use super::Handle;
+use super::Outputs;
 
 struct Inner {
     wl_surface: surface::Surface,
