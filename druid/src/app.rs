bfc54502e8f8418a5b6b13b48b1ba827d22cd288
jneem <joeneeman@gmail.com>
2021-11-26 09:06:29 -0600

    Reduce repetition.
    (as suggested by @PoignardAzur)

--- druid/src/app.rs
+++ druid/src/app.rs
@@ -183,8 +183,8 @@ impl<T: Data> AppLauncher<T> {
     ///
     /// # Panics
     ///
-    /// Panics if the subscriber fails to initialize.
-    /// It will raise a panic if a `tracing`/`tracing_wasm` global logger was already set.
+    /// Panics if the subscriber fails to initialize, for example if a `tracing`/`tracing_wasm`
+    /// global logger was already set.
     pub fn log_to_console(self) -> Self {
         #[cfg(not(target_arch = "wasm32"))]
         {
