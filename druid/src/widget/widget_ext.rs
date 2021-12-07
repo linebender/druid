14fd8fde08100af88530603b1ef64122aedd4d62
Smit <smit17xp@gmail.com>
2021-12-08 00:34:56 +0530

    Doc: fix LensWrap link (#2066)
    Explicit link is not needed
    
    Co-authored-by: jneem <joeneeman@gmail.com>
    Co-authored-by: Maan2003 <manmeetmann2003@gmail.com>

--- druid/src/widget/widget_ext.rs
+++ druid/src/widget/widget_ext.rs
@@ -232,10 +232,6 @@ pub trait WidgetExt<T: Data>: Widget<T> + Sized + 'static {
     }
 
     /// Wrap this widget in a [`LensWrap`] widget for the provided [`Lens`].
-    ///
-    ///
-    /// [`LensWrap`]: struct.LensWrap.html
-    /// [`Lens`]: trait.Lens.html
     fn lens<S: Data, L: Lens<S, T>>(self, lens: L) -> LensWrap<S, T, L, Self> {
         LensWrap::new(self, lens)
     }
