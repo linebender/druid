978f995a9dcbc99837e0c5c207a120dfefff6b4d
Gordon Shieh <gordon.shieh94@gmail.com>
2022-01-04 19:26:24 -0800

    Use 10% steps based off the min and max range of slider

--- druid/src/widget/slider.rs
+++ druid/src/widget/slider.rs
@@ -167,7 +167,7 @@ impl Slider {
         let increment = if let Some(step) = self.mapping.step {
             step
         } else {
-            0.1
+            (self.max - self.min) / 10.0
         };
 
         if mouse_delta < 0.0 {
