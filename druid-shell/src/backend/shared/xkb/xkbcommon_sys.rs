ac41a73f8ad37fc9e6d662c0321dd3c5007aab93
Joe Neeman <joeneeman@gmail.com>
2022-11-29 11:41:25 -0600

    Add xkbcommon path to build script.

--- druid-shell/src/backend/shared/xkb/xkbcommon_sys.rs
+++ druid-shell/src/backend/shared/xkb/xkbcommon_sys.rs
@@ -1,4 +1,4 @@
-#![allow(unused, non_upper_case_globals, non_camel_case_types)]
+#![allow(unused, non_upper_case_globals, non_camel_case_types, non_snake_case)]
 // unknown lints to make compile on older rust versions
 #![cfg_attr(test, allow(unknown_lints, deref_nullptr))]
 // generated code has some redundant static lifetimes, I don't think we can change that.
