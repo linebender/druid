e9b7f3863eed87b865f89fac160f20029ce136cd
John <giannis.sc@gmail.com>
2023-01-28 12:07:49 +0000

    Move semicolon to correct position

--- druid/src/widget/widget_ext.rs
+++ druid/src/widget/widget_ext.rs
@@ -309,9 +309,9 @@ mod tests {
         // this should be Container<Align<Container<Slider>>>
         let widget = Slider::new()
             .background(Color::BLACK)
-            .foreground(Color::WHITE);
+            .foreground(Color::WHITE)
             .align_left()
-            .border(Color::BLACK, 1.0)
+            .border(Color::BLACK, 1.0);
         assert!(widget.border_is_some());
         assert!(!widget.background_is_some());
         assert!(!widget.foreground_is_some());
