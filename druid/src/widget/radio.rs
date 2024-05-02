ebd4f598b2b17c000106bf7df2e94bb7e0463459
josefc12 <cermak.josef.12@email.cz>
2024-05-03 01:51:40 +0200

    radio now accepts size and padding for the individual Radio of the group.

--- druid/src/widget/radio.rs
+++ druid/src/widget/radio.rs
@@ -183,7 +183,7 @@ impl<T: Data + PartialEq> Widget<T> for Radio<T> {
         }
 
         // Paint the text label
-        self.child_label.draw_at(ctx, (size + x_padding, size));
+        self.child_label.draw_at(ctx, (size + x_padding, 0.0));
     }
 
     fn debug_state(&self, data: &T) -> DebugState {
